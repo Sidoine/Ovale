d12eb32b6a033f03b24aecd26991aa2e94c38eec
Sidoine De Wispelaere <sidoine@sidoine.net>
2009-04-23 18:31:02 +0000

    - added default for rogues
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@39 d5049fe3-3747-40f7-a4b5-f36d6801af5f

---
+++ defaut/Voleur.lua
@@ -0,0 +1,80 @@
+Ovale.defaut["ROGUE"] =
+[[
+Define(ENVENOM 32645)
+Define(TALENTVILEPOISONS 682)
+Define(TALENTCUTTOTHECHASE 2070)
+Define(SLICEANDDICE 5171)
+Define(RUPTURE 1943)
+Define(DEEPWOUNDS 12721)
+Define(REND 772)
+Define(RIP 1079)
+Define(HUNGERFORBLOOD 51662)
+Define(EVISCERATE 2098)
+Define(MUTILATE 1329)
+Define(SINISTERSTRIKE 1752)
+Define(ADRENALINERUSH 13750)
+Define(KILLINGSPREE 51690)
+Define(BLADEFLURRY 13877)
+Define(COLDBLOOD 14177)
+Define(PREPARATION 14185)
+Define(TRICKSOFTHETRADE 57934)
+Define(CLOACKOFSHADOWS 31224)
+
+AddIcon
+{
+	unless BuffPresent(SLICEANDDICE)
+	{
+		if ComboPoints(more 2)
+			Spell(SLICEANDDICE)
+	}
+	
+	unless TargetDebuffPresent(RUPTURE)
+	{
+		if ComboPoints(more 3)
+			Spell(RUPTURE)
+	}
+	
+	if {TargetDebuffPresent(RUPTURE) or TargetDebuffPresent(DEEPWOUNDS) or TargetDebuffPresent(REND)
+		or TargetDebuffPresent(RIP)} and BuffExpires(HUNGERFORBLOOD 2)
+		Spell(HUNGERFORBLOOD)
+		
+	if ComboPoints(more 3)
+	{
+		if BuffExpires(SLICEANDDICE 12) 
+		{
+			if TalentPoints(TALENTCUTTOTHECHASE more 0)
+				Spell(ENVENOM)
+			Spell(SLICEANDDICE)
+		}
+		if TalentPoints(TALENTVILEPOISONS more 0)
+			Spell(ENVENOM)
+		Spell(EVISCERATE)
+	}
+	
+	if ComboPoints(less 4)
+	{
+		Spell(MUTILATE)
+		Spell(SINISTERSTRIKE)
+	} 
+}
+
+AddIcon
+{
+	unless BuffPresent(KILLINGSPREE) Spell(ADRENALINERUSH)
+	unless BuffPresent(ADRENALINERUSH) Spell(KILLINGSPREE)
+	Spell(BLADEFLURRY)
+	Spell(COLDBLOOD)
+	Spell(PREPARATION)
+}
+
+AddIcon size=small
+{
+	Spell(TRICKSOFTHETRADE)
+}
+
+AddIcon size=small
+{
+	Spell(CLOACKOFSHADOWS)
+}
+
+]]
