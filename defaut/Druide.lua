704283628b7c3377615ef19cb838f2ad0f8fe573
Sidoine De Wispelaere <sidoine@sidoine.net>
2009-05-07 19:57:18 +0000

    - fixes for cat. should be fine now
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@55 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- defaut/Druide.lua
+++ defaut/Druide.lua
@@ -17,6 +17,11 @@ Define(WRATH 5176)
 Define(ECLIPSESTARFIRE 48518)
 Define(ECLIPSEWRATH 48517)
 Define(TIGERSFURY 5217)
+Define(FORCEOFNATURE 33831)
+Define(RAKE 59886)
+Define(SAVAGEROAR 52610)
+Define(FEROCIOUSBITE 22568)
+Define(BERSERK 50334)
 
 AddCheckBox(multi L(AOE))
 AddCheckBox(blood L(Blood))
@@ -25,57 +30,60 @@ AddCheckBox(lucioles SpellName(FAERIEFIRE))
 
 AddIcon
 {
-  if Stance(1)
-  {
-     if CheckBoxOn(lucioles) and TargetDebuffExpires(FAERIEFERAL 2)
-       Spell(FAERIEFERAL)
-    
-     if CheckBoxOn(demo) and TargetDebuffExpires(DEMOROAR 2)
-       Spell(DEMOROAR)
-        
-     Spell(MANGLEBEAR)
-     
-     if CheckBoxOn(blood) and Mana(more 10) and TargetDebuffExpires(LACERATE 4 stacks=5)
-       Spell(LACERATE)
-    
-     if CheckBoxOn(multi)
-       Spell(SWIPE)    
-  }
+	if Stance(1) # bear
+	{
+		if CheckBoxOn(lucioles) and TargetDebuffExpires(FAERIEFERAL 2)
+			Spell(FAERIEFERAL)
 
-  if Stance(3)
-  {
-     if CheckBoxOn(lucioles) and
-         TargetDebuffExpires(FAERIEFERAL 2)
-       Spell(FAERIEFERAL)
+		if CheckBoxOn(demo) and TargetDebuffExpires(DEMOROAR 2)
+			Spell(DEMOROAR)
 
-     Spell(TIGERSFURY)
+		Spell(MANGLEBEAR)
+
+		if CheckBoxOn(blood) and Mana(more 10) and TargetDebuffExpires(LACERATE 4 stacks=5)
+			Spell(LACERATE)
+
+		if CheckBoxOn(multi)
+			Spell(SWIPE)    
+	}
+
+	if Stance(3) # cat
+	{
+		if CheckBoxOn(lucioles) and	TargetDebuffExpires(FAERIEFERAL 2)
+			Spell(FAERIEFERAL)
+
     
-     if ComboPoints(more 4) and Mana(more 70)
-       Spell(RIP)
+		if ComboPoints(more 4) and Mana(more 70)
+		{
+			if BuffExpires(SAVAGEROAR 5) Spell(SAVAGEROAR)
+			if TargetDebuffExpires(RAKE 0) Spell(RAKE)
+			if TargetDebuffExpires(RIP 0) Spell(RIP)
+			Spell(FEROCIOUSBITE)
+		}
     
-     if ComboPoints(less 5)
-     {
-       if TargetDebuffExpires(MANGLECAT 0)
-          Spell(MANGLECAT)
-       Spell(SHRED)
-     }
-  }
+		if ComboPoints(less 5)
+		{
+			if Mana(less 40) Spell(TIGERSFURY)
+			if TargetDebuffExpires(MANGLECAT 0)
+				Spell(MANGLECAT)
+			Spell(SHRED)
+		}
+	}
 
-  unless Stance(1) or Stance(3)
-  {
-     if CheckBoxOn(lucioles) and 
-         TargetDebuffExpires(FAERIEFIRE 2)
-      Spell(FAERIEFIRE)
-       
-     if TargetDebuffExpires(MOONFIRE 0)
-        Spell(MOONFIRE)
-     if TargetDebuffExpires(INSECTSWARM 0)
-       Spell(INSECTSWARM)  
+	unless Stance(1) or Stance(3)
+	{
+		if CheckBoxOn(lucioles) and TargetDebuffExpires(FAERIEFIRE 2)
+		Spell(FAERIEFIRE)
 
-     unless BuffPresent(ECLIPSEWRATH)
-        Spell(STARFIRE)
-     Spell(WRATH)
-  }
+		if TargetDebuffExpires(MOONFIRE 0)
+			Spell(MOONFIRE)
+		if TargetDebuffExpires(INSECTSWARM 0)
+			Spell(INSECTSWARM)  
+
+		unless BuffPresent(ECLIPSEWRATH)
+			Spell(STARFIRE)
+		Spell(WRATH)
+	}
 }
 
 AddIcon
@@ -86,5 +94,10 @@ AddIcon
   }
 }
 
+AddIcon
+{
+	Spell(FORCEOFNATURE)
+	Spell(BERSERK)
+}
 
 ]]
