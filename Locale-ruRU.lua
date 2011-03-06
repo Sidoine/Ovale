c292595323f856e93cec5c5b20321cd6c1e9bcbd
Sidoine De Wispelaere <sidoine@sidoine.net>
2011-03-06 16:35:43 +0000

    added russian translation
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@368 d5049fe3-3747-40f7-a4b5-f36d6801af5f

---
+++ Locale-ruRU.lua
@@ -0,0 +1,4 @@
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Ovale", "ruRU", false)
+if not L then return end
+
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
