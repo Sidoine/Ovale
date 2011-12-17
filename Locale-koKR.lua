72b328a147dd7c6a82224d74658e8ff188875237
Sidoine De Wispelaere <sidoine@sidoine.net>
2011-12-17 12:30:18 +0000

    kOKR file
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@447 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- Locale-koKR.lua
+++ Locale-koKR.lua
@@ -1,4 +1,4 @@
-local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Ovale", "ruRU", false)
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Ovale", "koKR", false)
 if not L then return end
 
---@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
