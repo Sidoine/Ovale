6a108f871eb75395877495274a3f90d69ca497c7
Johnny C. Lam <jlam88@gmail.com>
2012-09-24 20:01:27 +0000

    Strip extra leading and trailing parens from @paramsig.
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@557 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- OvaleCondition.lua
+++ OvaleCondition.lua
@@ -422,7 +422,7 @@ OvaleCondition.conditions=
 	end,]]
 	--- Get how many pieces of an armor set, e.g., Tier 14 set, are equipped by the player.
 	-- @name ArmorSetParts
-	-- @paramsig (setName [operator number])
+	-- @paramsig setName [operator number]
 	-- @param setName The name of the armor set. Valid values are: T11, T12, T13.
 	-- @param operator (Optional) Comparison operator: equal, less, more.
 	-- @param number (Optional) The number to compare against.
