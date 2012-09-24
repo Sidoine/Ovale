4f244afdd26e452042be330fbec5d1e7661700f4
Johnny C. Lam <jlam88@gmail.com>
2012-09-24 20:18:53 +0000

    Test dropping @name from LuaDoc comments.
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@561 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- OvaleCondition.lua
+++ OvaleCondition.lua
@@ -421,17 +421,16 @@ OvaleCondition.conditions=
 		end 
 	end,]]
 --- Get how many pieces of an armor set, e.g., Tier 14 set, are equipped by the player.
--- @name ArmorSetParts
--- @paramsig ArmorSetParts(setName [operator number])
+-- @paramsig setName [operator number]
 -- @param setName The name of the armor set. Valid values are: T11, T12, T13.
 -- @param operator (Optional) Comparison operator: equal, less, more.
 -- @param number (Optional) The number to compare against.
 -- @returns A number or a boolean value.
 -- @usage
 -- if ArmorSetParts(T13) >=2 and target.HealthPercent() <60
---	Spell(ferocious_bite)
+--     Spell(ferocious_bite)
 -- if ArmorSetParts(T13 more 1) and TargetHealthPercent(less 60)
---	Spell(ferocious_bite)
+--     Spell(ferocious_bite)
 	armorsetparts = function(condition)
 		local nombre = 0
 		if OvaleEquipement.nombre[condition[1]] then
