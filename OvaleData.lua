bfcfbf204fe29418e68267b8a2dbb0cd4f407565
Johnny C. Lam <jlam88@gmail.com>
2013-04-23 17:21:33 +0000

    Add support for Ji-Kun Primal Nutrient buff. From @aggixx.
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@909 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- OvaleData.lua
+++ OvaleData.lua
@@ -97,6 +97,7 @@ OvaleData.selfDamageBuff =
 	[84747] = 1.3, -- Deep Insight (rogue)
 	[124974] = 1.2, -- Nature's Vigil (druid)
 	[127538] = 1.3, -- Savage Roar (druid)
+	[140741] = 2.0, -- Primal Nutriment (Throne of Thunder, Ji-Kun encounter)
 }
 
 OvaleData.buffSpellList =
