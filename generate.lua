78186f738fe4c35de1ed8c205dd3606bf537f54b
Johnny C. Lam <jlam88@gmail.com>
2014-07-13 11:30:30 +0000

    Fix generate.lua to work after WoWAPI module changes.
    git-svn-id: svn://svn.curseforge.net/wow/ovale/mainline/trunk@1534 d5049fe3-3747-40f7-a4b5-f36d6801af5f

--- generate.lua
+++ generate.lua
@@ -1,38 +1,53 @@
+--[[------------------------------
+	Load fake WoW environment.
+--]]------------------------------
+local root = "../"
 do
+	local state = {
+		class = "DRUID",
+	}
 	dofile("WoWAPI.lua")
+	WoWAPI:Initialize("Ovale", state)
+	WoWAPI:ExportSymbols()
+end
 
-	Ovale = {}
-	LoadAddonFile("OvaleLexer.lua")
-	LoadAddonFile("OvaleSimulationCraft.lua")
-	local OvaleSimulationCraft = Ovale.OvaleSimulationCraft
+do
+	-- Load all of the addon files.
+	WoWAPI:LoadAddonFile("Ovale.toc")
 
-	local profilesDirectory = "..\\SimulationCraft\\profiles\\Tier16H"
-	local outputDirectory = "scripts"
+	-- Pretend to fire ADDON_LOADED event.
+	local AceAddon = LibStub("AceAddon-3.0")
+	AceAddon:ADDON_LOADED()
+end
 
-	local saveInput = io.input()
-	local saveOutput = io.output()
+local OvaleSimulationCraft = Ovale.OvaleSimulationCraft
 
-	local dir = io.popen("dir /b " .. profilesDirectory)
-	os.execute("mkdir " .. outputDirectory)
-	for filename in dir:lines() do
-		if string.match(filename, "^[A-Z]") then
-			local inputName = string.gsub(profilesDirectory, "\\", "/") .. "/" .. filename
-			io.input(inputName)
-			local simcStr = io.read("*all")
-			if not string.find(simcStr, "optimal_raid=") then
-				local simc = OvaleSimulationCraft(simcStr)
-				simc.simcComments = true
+local profilesDirectory = "..\\SimulationCraft\\profiles\\Tier16H"
+local outputDirectory = "scripts"
 
-				local outputFileName = "simulationcraft_" .. string.lower(string.gsub(filename, ".simc", ".lua"))
-				outputFileName = string.gsub(outputFileName, "death_knight", "deathknight")
-				print("Generating " .. outputFileName)
-				local outputName = outputDirectory .. "/" .. outputFileName
-				io.output(outputName)
-				io.write(table.concat(simc:GenerateScript(), "\n"))
-			end
+local saveInput = io.input()
+local saveOutput = io.output()
+
+local dir = io.popen("dir /b " .. profilesDirectory)
+os.execute("mkdir " .. outputDirectory)
+for filename in dir:lines() do
+	if string.match(filename, "^[A-Z]") then
+		local inputName = string.gsub(profilesDirectory, "\\", "/") .. "/" .. filename
+		io.input(inputName)
+		local simcStr = io.read("*all")
+		if not string.find(simcStr, "optimal_raid=") then
+			local simc = OvaleSimulationCraft(simcStr)
+			simc.simcComments = true
+
+			local outputFileName = "simulationcraft_" .. string.lower(string.gsub(filename, ".simc", ".lua"))
+			outputFileName = string.gsub(outputFileName, "death_knight", "deathknight")
+			print("Generating " .. outputFileName)
+			local outputName = outputDirectory .. "/" .. outputFileName
+			io.output(outputName)
+			io.write(table.concat(simc:GenerateScript(), "\n"))
 		end
 	end
-
-	io.input(saveInput)
-	io.output(saveOutput)
 end
+
+io.input(saveInput)
+io.output(saveOutput)
