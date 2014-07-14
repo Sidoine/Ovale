8f9870074739585a7245f5d9f54c27490f1513b4
Johnny C. Lam <jlam88@gmail.com>
2014-07-13 21:36:29 -0400

    Add ExecuteTime() script condition to match SimC's "execute_time".
    This condition returns the larger of a spell's cast time or the GCD.

--- conditions/CastTime.lua
+++ conditions/CastTime.lua
@@ -12,9 +12,20 @@ local _, Ovale = ...
 
 do
 	local OvaleCondition = Ovale.OvaleCondition
+	local OvaleCooldown = Ovale.OvaleCooldown
 
 	local API_GetSpellInfo = GetSpellInfo
 	local Compare = OvaleCondition.Compare
+
+	local function GetCastTime(spellId)
+		local _, _, _, _, _, _, castTime = API_GetSpellInfo(spellId)
+		if castTime then
+			castTime = castTime / 1000
+		else
+			castTime = 0
+		end
+		return castTime
+	end
 
 	--- Get the cast time in seconds of the spell for the player, taking into account current haste effects.
 	-- @name CastTime
@@ -22,28 +33,40 @@ do
 	-- @param id The spell ID.
 	-- @param operator Optional. Comparison operator: less, atMost, equal, atLeast, more.
 	-- @param number Optional. The number to compare against.
-	-- @param target Optional. Sets the target to check. The target may also be given as a prefix to the condition.
-	--     Defaults to target=player.
-	--     Valid values: player, target, focus, pet.
 	-- @return The number of seconds.
 	-- @return A boolean value for the result of the comparison.
-	-- @see RemainingCastTime
+	-- @see ExecuteTime
 	-- @usage
 	-- if target.DebuffRemains(flame_shock) < CastTime(lava_burst)
 	--     Spell(lava_burst)
 
 	local function CastTime(condition)
 		local spellId, comparator, limit = condition[1], condition[2], condition[3]
-		local castTime = 0
-		if spellId then
-			local _, _, _, _, _, _, _castTime = API_GetSpellInfo(spellId)
-			if _castTime then
-				castTime = _castTime / 1000
-				Ovale:Logf("castTime = %f %s %s", castTime, comparator, limit)
-			end
-		end
+		local castTime = GetCastTime(spellId)
 		return Compare(castTime, comparator, limit)
 	end
 
+	--- Get the cast time in seconds of the spell for the player or the GCD for the player, whichever is greater.
+	-- @name ExecuteTime
+	-- @paramsig number or boolean
+	-- @param id The spell ID.
+	-- @param operator Optional. Comparison operator: less, atMost, equal, atLeast, more.
+	-- @param number Optional. The number to compare against.
+	-- @return The number of seconds.
+	-- @return A boolean value for the result of the comparison.
+	-- @see CastTime
+	-- @usage
+	-- if target.DebuffRemains(flame_shock) < ExecuteTime(lava_burst)
+	--     Spell(lava_burst)
+
+	local function ExecuteTime(condition)
+		local spellId, comparator, limit = condition[1], condition[2], condition[3]
+		local castTime = GetCastTime(spellId)
+		local gcd = OvaleCooldown:GetGCD()
+		local t = (castTime > gcd) and castTime or gcd
+		return Compare(t, comparator, limit)
+	end
+
 	OvaleCondition:RegisterCondition("casttime", true, CastTime)
+	OvaleCondition:RegisterCondition("executetime", true, ExecuteTime)
 end
