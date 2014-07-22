4db4709d0aa6e9d6fc04d76017fb251207c599df
Johnny C. Lam <jlam88@gmail.com>
2014-07-22 10:39:08 -0400

    Fix copyright dates for files touched in 2014.

--- WoWAPI.lua
+++ WoWAPI.lua
@@ -1,6 +1,6 @@
 --[[--------------------------------------------------------------------
     Ovale Spell Priority
-    Copyright (C) 2013 Johnny C. Lam
+    Copyright (C) 2013, 2014 Johnny C. Lam
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License in the LICENSE
