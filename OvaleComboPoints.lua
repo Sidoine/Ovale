240197d791dc69bf5ab75d09b0e59f2b439fdeb8
Johnny C. Lam <jlam88@gmail.com>
2014-10-14 17:05:59 -0400

    Change OvaleComboPoints to use a queue of events to track combo points.
    Every spell that can cause a change in the number of combo points creates
    pending combo point events that are queued when that spell is cast.
    
    Every actual combo point change, tracked through UNIT_COMBO_POINTS events,
    removes the corresponding pending combo point event.
    
    A new method OvaleComboPoints:GetComboPoints() returns the total number of
    combo points, taking into account any pending combo point events.  This
    total "looks ahead" and grants the combo points from Ruthlessness, Seal
    Fate, Primal Fury, and Anticipation before they are reflected in the game
    API.

--- OvaleComboPoints.lua
+++ OvaleComboPoints.lua
@@ -3,7 +3,7 @@
     See the file LICENSE.txt for copying permission.
 --]]--------------------------------------------------------------------
 
--- This addon tracks the number of combo points by the player on the current target.
+-- This addon tracks the number of combo points on the player.
 
 local _, Ovale = ...
 local OvaleComboPoints = Ovale:NewModule("OvaleComboPoints", "AceEvent-3.0")
@@ -28,7 +28,10 @@ local OvaleSpellBook = nil
 local OvaleState = nil
 
 local API_GetComboPoints = GetComboPoints
+local API_GetTime = GetTime
+local API_UnitCanAttack = UnitCanAttack
 local API_UnitClass = UnitClass
+local API_UnitExists = UnitExists
 local API_UnitGUID = UnitGUID
 local MAX_COMBO_POINTS = MAX_COMBO_POINTS
 
@@ -42,16 +45,15 @@ local ANTICIPATION = 115189
 local ANTICIPATION_DURATION = 15
 local ANTICIPATION_TALENT = 18
 local self_hasAnticipation = false
--- The number of stacks of Anticipation that were on the player when the most recent finisher was cast.
-local self_anticipation = 0
--- Rogue offensive finishers.
-local OFFENSIVE_FINISHER_ROGUE = {
-	[   408] = "Kidney Shot",
-	[  1943] = "Rupture",
-	[  2098] = "Eviscerate",
-	[ 32645] = "Envenom",
-	[121411] = "Crimson Tempest",
-}
+
+-- Rogue's Ruthlessness passive spell.
+local RUTHLESSNESS = 14161
+local self_hasRuthlessness = false
+
+-- Queue of pending combo point events.
+local self_pendingComboEvents = {}
+-- Number of seconds a pending combo point event can exist without expiring.
+local PENDING_THRESHOLD = 0.8
 
 -- Table of functions to update spellcast information to register with OvaleFuture.
 local self_updateSpellcastInfo = {}
@@ -60,10 +62,46 @@ local OVALE_COMBO_POINTS_DEBUG = "combo_points"
 --</private-static-properties>
 
 --<public-static-properties>
+-- The current number of combo points on the player.
 OvaleComboPoints.combo = 0
+-- The GUID of the last target to yield valid combo point information.
+OvaleComboPoints.comboGUID = nil
 --</public-static-properties>
 
 --<private-static-methods>
+-- Add a pending combo event caused by the given spell.
+local function AddPendingComboEvent(atTime, spellId, guid, reason, combo)
+	local comboEvent = {
+		atTime = atTime,
+		spellId = spellId,
+		guid = guid,
+		reason = reason,
+		combo = combo,
+	}
+	tinsert(self_pendingComboEvents, comboEvent)
+end
+
+-- Remove all pending combo point events caused by the given spell on the target GUID.
+-- If only atTime is given, then all expired events are removed.
+local function RemovePendingComboEvents(atTime, spellId, guid, reason, combo)
+	local count = 0
+	for k = #self_pendingComboEvents, 1, -1 do
+		local comboEvent = self_pendingComboEvents[k]
+		-- Remove expired or matching pending events.
+		if (atTime and atTime - comboEvent.atTime > PENDING_THRESHOLD)
+				or (comboEvent.spellId == spellId and comboEvent.guid == guid and (not reason or comboEvent.reason == reason) and (not combo or comboEvent.combo == combo)) then
+			if comboEvent.combo == "finisher" then
+				Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "Removing expired %s event: spell %d combo point finisher from %s.", comboEvent.reason, comboEvent.spellId, comboEvent.reason)
+			else
+				Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "Removing expired %s event: spell %d for %d combo points from %s.", comboEvent.reason, comboEvent.spellId, comboEvent.combo, comboEvent.reason)
+			end
+			count = count + 1
+			tremove(self_pendingComboEvents, k)
+		end
+	end
+	return count
+end
+
 -- Manage spellcast.combo information.
 local function SaveToSpellcast(spellcast)
 	if spellcast.spellId then
@@ -110,16 +148,11 @@ end
 function OvaleComboPoints:OnEnable()
 	self_guid = API_UnitGUID("player")
 	if self_class == "ROGUE" or self_class == "DRUID" then
-		self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-		self:RegisterEvent("PLAYER_ENTERING_WORLD", "Refresh")
-		self:RegisterEvent("PLAYER_LOGIN", "Refresh")
-		self:RegisterEvent("PLAYER_TARGET_CHANGED", "Refresh")
+		self:RegisterEvent("PLAYER_ENTERING_WORLD", "Update")
+		self:RegisterEvent("PLAYER_TARGET_CHANGED")
 		self:RegisterEvent("UNIT_COMBO_POINTS")
-		self:RegisterEvent("UNIT_TARGET", "UNIT_COMBO_POINTS")
-		if self_class == "ROGUE" then
-			self:RegisterMessage("Ovale_AuraRemoved")
-			self:RegisterMessage("Ovale_TalentsChanged")
-		end
+		self:RegisterMessage("Ovale_SpellFinished")
+		self:RegisterMessage("Ovale_TalentsChanged")
 		OvaleState:RegisterState(self, self.statePrototype)
 		OvaleFuture:RegisterSpellcastInfo(self_updateSpellcastInfo)
 	end
@@ -129,99 +162,130 @@ function OvaleComboPoints:OnDisable()
 	if self_class == "ROGUE" or self_class == "DRUID" then
 		OvaleState:UnregisterState(self)
 		OvaleFuture:UnregisterSpellcastInfo(self_updateSpellcastInfo)
-		self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-		self:UnregisterEvent("PLAYER_LOGIN")
 		self:UnregisterEvent("PLAYER_TARGET_CHANGED")
 		self:UnregisterEvent("UNIT_COMBO_POINTS")
-		self:UnregisterEvent("UNIT_TARGET")
-		if self_class == "ROGUE" then
-			self:RegisterMessage("Ovale_AuraRemoved")
-			self:RegisterMessage("Ovale_TalentsChanged")
-		end
+		self:UnregisterMessage("Ovale_SpellFinished")
+		self:UnregisterMessage("Ovale_TalentsChanged")
 	end
 end
 
---[[
-	A rogue's Seal Fate or a druid's Primal Fury are passive abilities that grant an
-	extra combo point when a combo-point generator critically strikes the target.
-
-	Workaround the "combo point delay" after a generator critically strikes the target
-	by catching the critical strike damage event and adding the given number of extra
-	combo points.  The delay MUST be less than the GCD.
-
-	An ability that generates extra combo points after it critically strikes the target
-	should have a "critcombo=N" parameter in its SpellInfo() description, where N is
-	the number of extra combo points to add, e.g., critcombo=1.
---]]
-function OvaleComboPoints:COMBAT_LOG_EVENT_UNFILTERED(event, timestamp, cleuEvent, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, ...)
-	local arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25 = ...
-
-	if sourceGUID == self_guid and destGUID == OvaleGUID:GetGUID("target") then
-		if cleuEvent == "SPELL_DAMAGE" then
-			local spellId, critical = arg12, arg21
-			local si = OvaleData.spellInfo[spellId]
-			if critical and si and si.critcombo then
-				Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "Critical strike for %d additional combo points.", si.critcombo)
-				self.combo = self.combo + si.critcombo
-				if self.combo > MAX_COMBO_POINTS then
-					self.combo = MAX_COMBO_POINTS
-				end
-			end
-		elseif self_hasAnticipation and cleuEvent == "SPELL_CAST_SUCCESS" then
-			local spellId = arg12
-			if OFFENSIVE_FINISHER_ROGUE[spellId] then
-				local aura = OvaleAura:GetAuraByGUID(self_guid, ANTICIPATION, "HELPFUL", true)
-				if OvaleAura:IsActiveAura(aura) then
-					self_anticipation = aura.stacks
-					Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "Finisher with %d anticipation stacks.", self_anticipation)
-				end
-			end
-		end
+function OvaleComboPoints:PLAYER_TARGET_CHANGED(event, cause)
+	if cause == "NIL" or cause == "down" then
+		-- Target was cleared.
+	else
+		-- Target has changed.
+		self:Update(true)
 	end
 end
 
-function OvaleComboPoints:UNIT_COMBO_POINTS(event, ...)
-	local unitId = ...
+function OvaleComboPoints:UNIT_COMBO_POINTS(event, unitId)
 	if unitId == "player" then
-		self:Refresh()
+		-- Save the old combo point count and update to the current count.
+		local oldCombo = self.combo
+		self:Update()
+
+		local now = API_GetTime()
+		local difference = self.combo - oldCombo
+		Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "%s (%f): %d -> %d.", event, now, oldCombo, self.combo)
+
+		-- Remove expired events.
+		RemovePendingComboEvents(now)
+
+		local pendingMatched = false
+		if #self_pendingComboEvents > 0 then
+			local comboEvent = self_pendingComboEvents[1]
+			local spellId, guid, reason, combo = comboEvent.spellId, comboEvent.guid, comboEvent.reason, comboEvent.combo
+			if guid == self.comboGUID or guid == self_guid or not Ovale.enCombat then
+				if combo == difference or (combo == "finisher" and self.combo == 0 and difference < 0) then
+					Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Matches pending %s event for %d.", reason, spellId)
+					pendingMatched = true
+					tremove(self_pendingComboEvents, 1)
+				end
+			end
+		end
+		if not pendingMatched and not Ovale.enCombat and difference <= 0 then
+			Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Out-of-combat combo point decay.")
+			if difference == 0 then
+				-- Decrement the combo point count until the next time a hostile unit is targeted to get a true update.
+				local newCombo = self.combo - 1
+				self.combo = newCombo > 0 and newCombo or 0
+				Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Decaying to %d combo point(s).", self.combo)
+			end
+		end
 	end
 end
 
-function OvaleComboPoints:Ovale_AuraRemoved(event, atTime, guid, auraId, source)
-	if guid == self_guid and auraId == ANTICIPATION then
-		self_anticipation = 0
+function OvaleComboPoints:Ovale_SpellFinished(event, atTime, spellId, targetGUID, success)
+	Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "%s (%f): Spell %d finished (%s) on %s", event, atTime, spellId, success, targetGUID)
+	local si = OvaleData.spellInfo[spellId]
+	if si and si.combo and (success == "hit" or success == "critical") then
+		if si.combo == "finisher" then
+			Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Spell %d hit and consumed all combo points.", spellId)
+			AddPendingComboEvent(atTime, spellId, targetGUID, "finisher", si.combo)
+			if self_hasRuthlessness and self.combo == MAX_COMBO_POINTS then
+				-- Ruthlessness grants a 20% chance to grant a combo point for each combo point spent on a finishing move.
+				Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Spell %d has 100% chance to grant an extra combo point from Ruthlessness.", spellId)
+				AddPendingComboEvent(atTime, spellId, targetGUID, "Ruthlessness", 1)
+			end
+			if self_hasAnticipation and targetGUID ~= self_guid then
+				-- Anticipation causes offensive finishing moves to consume all Anticipation charges and to grant a combo point for each.
+				local unitId = OvaleGUID:GetUnitId(targetGUID)
+				if unitId and API_UnitCanAttack("player", unitId) then
+					local aura = OvaleAura:GetAuraByGUID(self_guid, ANTICIPATION, "HELPFUL", true)
+					if OvaleAura:IsActiveAura(aura) then
+						Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "    Spell %d hit with %d Anticipation charges.", spellId, aura.stacks)
+						AddPendingComboEvent(atTime, spellId, targetGUID, "Anticipation", aura.stacks)
+					end
+				end
+			end
+		end
 	end
 end
 
 function OvaleComboPoints:Ovale_TalentsChanged(event)
-	self_hasAnticipation = (self_class == "ROGUE" and OvaleSpellBook:GetTalentPoints(ANTICIPATION_TALENT) > 0)
+	if self_class == "ROGUE" then
+		self_hasAnticipation = OvaleSpellBook:GetTalentPoints(ANTICIPATION_TALENT) > 0
+		self_hasRuthlessness = OvaleSpellBook:IsKnownSpell(RUTHLESSNESS)
+	end
 end
 
-function OvaleComboPoints:Refresh()
-	profiler.Start("OvaleComboPoints_Refresh")
-	local combo = API_GetComboPoints("player") or 0
-	local oldCombo = self.combo
-	if oldCombo == combo then
-		-- Game state has caught up with the adjusted combo point total, so remove the adjustment.
-		self_anticipation = 0
-	else
-		if self_hasAnticipation and self_anticipation > 0 then
-			self.combo = combo + self_anticipation
-			if self.combo > MAX_COMBO_POINTS then
-				self.combo = MAX_COMBO_POINTS
-			end
-			Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "%d -> %d (+%d).", oldCombo, combo, self_anticipation)
-		else
-			self.combo = combo
-			Ovale:DebugPrintf(OVALE_COMBO_POINTS_DEBUG, "%d -> %d.", oldCombo, combo)
+function OvaleComboPoints:Update(hasTarget)
+	profiler.Start("OvaleComboPoints_Update")
+	if hasTarget or API_UnitExists("target") then
+		local canAttack = API_UnitCanAttack("player", "target")
+		if canAttack then
+			self.combo = API_GetComboPoints("player", "target")
+			self.comboGUID = API_UnitGUID("target")
 		end
 	end
-	profiler.Stop("OvaleComboPoints_Refresh")
+	profiler.Stop("OvaleComboPoints_Update")
+end
+
+function OvaleComboPoints:GetComboPoints()
+	-- Remove expired events.
+	local now = API_GetTime()
+	RemovePendingComboEvents(now)
+
+	-- Start with the true combo point total and adjust for any pending combo points.
+	local total = self.combo
+	for k = 1, #self_pendingComboEvents do
+		local combo = self_pendingComboEvents[k].combo
+		if combo == "finisher" then
+			total = 0
+		else -- if type(combo) == "number" then
+			total = total + combo
+		end
+		-- Clamp combo points to the maximum.
+		if total > MAX_COMBO_POINTS then
+			total = MAX_COMBO_POINTS
+		end
+	end
+	return total
 end
 
 function OvaleComboPoints:Debug()
-	Ovale:FormatPrint("Player has %d combo points on target %s.", self.combo, OvaleGUID:GetGUID("target"))
+	Ovale:FormatPrint("Player has %d combo points.", self.combo)
 end
 --</public-static-methods>
 
@@ -250,7 +314,15 @@ end
 -- Reset the state to the current conditions.
 function OvaleComboPoints:ResetState(state)
 	profiler.Start("OvaleComboPoints_ResetState")
-	state.combo = self.combo or 0
+	state.combo = self:GetComboPoints()
+	-- Scan the pending combo point events and remove the Anticipation buff if there is pending Anticipation event.
+	for k = 1, #self_pendingComboEvents do
+		local comboEvent = self_pendingComboEvents[k]
+		if comboEvent.reason == "Anticipation" then
+			state:RemoveAuraOnGUID(self_guid, ANTICIPATION, "HELPFUL", true, comboEvent.atTime)
+			break
+		end
+	end
 	profiler.Stop("OvaleComboPoints_ResetState")
 end
 
@@ -265,27 +337,36 @@ function OvaleComboPoints:ApplySpellAfterCast(state, spellId, targetGUID, startC
 		-- Clamp combo points to lower and upper limits.
 		if power <= 0 then
 			power = 0
-			--[[
-				If a rogue is talented into Anticipation, then any stacks of Anticipation
-				become combo points on the target after the finisher is cast.
-			--]]
+			-- Ruthlessness grants a 20% chance to grant a combo point for each combo point spent on a finishing move.
+			if self_hasRuthlessness and self.combo == MAX_COMBO_POINTS then
+				Ovale:Logf("Spell %d grants one extra combo point from Ruthlessness.", spellId)
+				power = power + 1
+			end
+			-- Anticipation causes offensive finishing moves to consume all Anticipation charges and to grant a combo point for each.
 			if self_hasAnticipation and state.combo > 0 then
 				local aura = state:GetAuraByGUID(self_guid, ANTICIPATION, "HELPFUL", true)
 				if state:IsActiveAura(aura, endCast) then
-					power = aura.stacks
+					power = power + aura.stacks
 					state:RemoveAuraOnGUID(self_guid, ANTICIPATION, "HELPFUL", true, state.currentTime)
+					-- Anticipation charges that are consumed to grant combo points don't overflow into new Anticipation charges.
+					if power > MAX_COMBO_POINTS then
+						power = MAX_COMBO_POINTS
+					end
 				end
 			end
 		end
 		if power > MAX_COMBO_POINTS then
 			--[[
 				If a rogue is talented into Anticipation, then any combo points over
-				MAX_COMBO_POINTS are added to the stacks of Anticipation on the player
-				to a maximum of MAX_COMBO_POINTS stacks.
+				MAX_COMBO_POINTS are added to the Anticipation charges on the player
+				to a maximum of MAX_COMBO_POINTS charges.
+
+				If a spell is flagged with "temp_combo=1", then any combo points it
+				grants cannot overflow into Anticipation charges.
 			--]]
-			if self_hasAnticipation then
+			if self_hasAnticipation and not si.temp_combo then
 				local stacks = power - MAX_COMBO_POINTS
-				-- Look for a pre-existing Anticipation buff and add to its stacks.
+				-- Look for a pre-existing Anticipation buff and add to its stack count.
 				local aura = state:GetAuraByGUID(self_guid, ANTICIPATION, "HELPFUL", true)
 				if state:IsActiveAura(aura, endCast) then
 					stacks = stacks + aura.stacks
