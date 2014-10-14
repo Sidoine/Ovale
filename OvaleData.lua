25f48cbc5f56b1152835d7f3f81ba7cbd6aa198b
Johnny C. Lam <jlam88@gmail.com>
2014-10-14 19:05:25 -0400

    Update buff/debuff lists in OvaleData for patch 6.0.

--- OvaleData.lua
+++ OvaleData.lua
@@ -8,33 +8,6 @@ local _, Ovale = ...
 local OvaleData = Ovale:NewModule("OvaleData")
 Ovale.OvaleData = OvaleData
 
---<private-static-properties>
--- Auras that are refreshed by spells that don't trigger a new snapshot.
-self_buffNoSnapshotSpellList =
-{
-	-- Rip (druid)
-	[1079] =
-	{
-		[5221] = true,		-- Shred
-		[6785] = true,		-- Ravage
-		[22568] = true,		-- Ferocious Bite (target below 25%)
-		[33876] = true,		-- Mangle
-		[102545] = true,	-- Ravage!
-		[114236] = true,	-- Shred!
-	},
-	-- Blood Plague (death knight)
-	[55078] =
-	{
-		[85948] = true,		-- Festering Strike (unholy)
-	},
-	-- Frost Fever (death knight)
-	[55095] =
-	{
-		[85948] = true,		-- Festering Strike (unholy)
-	},
-}
---</private-static-properties>
-
 --<public-static-properties>
 OvaleData.itemList = {}
 --spell info from the current script (by spellId)
@@ -44,195 +17,169 @@ OvaleData.buffSpellList =
 {
 	-- Debuffs
 	fear_debuff = {
-		[5782] = true, -- Fear
-		[5484] = true, -- Howl of terror
-		[5246] = true, -- Intimidating Shout
-		[8122] = true, -- Psychic scream
+		[  5246] = true, -- Intimidating Shout
+		[  5484] = true, -- Howl of terror
+		[  5782] = true, -- Fear
+		[  8122] = true, -- Psychic scream
 	},
 	incapacitate_debuff = {
-		[6770] = true, -- Sap
-		[12540] = true, -- Gouge
-		[20066] = true, -- Repentance
+		[  6770] = true, -- Sap
+		[ 12540] = true, -- Gouge
+		[ 20066] = true, -- Repentance
+		[137460] = true, -- Incapacitated
 	},
 	root_debuff = {
-		[23694] = true, -- Improved Hamstring
-		[339] = true, -- Entangling Roots
-		[122] = true, -- Frost Nova
-		[47168] = true, -- Improved Wing Clip
+		[   122] = true, -- Frost Nova
+		[   339] = true, -- Entangling Roots
 	},
 	stun_debuff = {
-		[5211] = true, -- Bash
-		[44415] = true, -- Blackout
-		[6409] = true, -- Cheap Shot
-		[22427] = true, -- Concussion Blow
-		[853] = true, -- Hammer of Justice
-		[408] = true, -- Kidney Shot
-		[46968] = true, -- Shockwave
+		[   408] = true, -- Kidney Shot
+		[   853] = true, -- Hammer of Justice
+		[  1833] = true, -- Cheap Shot
+		[  5211] = true, -- Mighty Bash
+		[ 46968] = true, -- Shockwave
 	},
 
 	-- Raid buffs
 	attack_power_multiplier_buff = {
-		[6673] = true, -- Battle Shout (warrior)
-		[19506] = true, -- Trueshot Aura (hunter)
-		[57330] = true, -- Horn of Winter (death knight)
-	},
-	attack_speed_buff = {
-		[30809] = true, -- Unleashed Rage (enhancement shaman)
-		[55610] = true, -- Unholy Aura (frost & unholy death knights)
-		[113742] = true, -- Swiftblade's Cunning (rogue)
-		[128432] = true, -- Cackling Howl (hyena)
-		[128433] = true, -- Serpent's Swiftness (serpent)
+		[  6673] = true, -- Battle Shout (warrior)
+		[ 19506] = true, -- Trueshot Aura (hunter)
+		[ 57330] = true, -- Horn of Winter (death knight)
 	},
 	critical_strike_buff = {
-		[1459] = true, -- Arcane Brillance (mage)
-		[24604] = true, -- Furious Howl (wolf)
-		[24932] = true, -- Leader of the Pack (feral & guardian druids)
-		[61316] = true, -- Dalaran Brilliance (mage)
-		[90309] = true, -- Terrifying Roar (devilsaur)
-		[97229] = true, -- Bellowing Roar (hydra)
-		[116781] = true, -- Legacy of the White Tiger (windwalker monk)
-		[126309] = true, -- Still Water (waterstrider)
+		[  1459] = true, -- Arcane Brillance (mage)
+		[ 24604] = true, -- Furious Howl (wolf)
+		[ 24932] = true, -- Leader of the Pack (feral druid)
+		[ 61316] = true, -- Dalaran Brilliance (mage)
+		[ 90309] = true, -- Terrifying Roar (devilsaur)
+		[ 90363] = true, -- Embrace of the Shale Spider (shale spider)
+		[ 97229] = true, -- Bellowing Roar (hydra)
+		[116781] = true, -- Legacy of the White Tiger (bremaster/windwalker monk)
+		[126309] = true, -- Still Water (water strider)
 		[126373] = true, -- Fearless Roar (quilen)
+		[160052] = true, -- Strength of the Pack (raptor)
+	},
+	haste_buff = {
+		[ 49868] = true, -- Mind Quickening (shadow priest)
+		[ 55610] = true, -- Unholy Aura (frost/unholy death knight)
+		[113742] = true, -- Swiftblade's Cunning (rogue)
+		[128432] = true, -- Cackling Howl (hyena)
+		[135678] = true, -- Energizing Spores (sporebat)
+		[160003] = true, -- Savage Vigor (rylak)
+		[160074] = true, -- Speed of the Swarm (wasp)
 	},
 	mastery_buff = {
-		[19740] = true, -- Blessing of Might (paladin)
-		[93435] = true, -- Roar of Courage (cat)
+		[ 19740] = true, -- Blessing of Might (paladin)
+		[ 24907] = true, -- Moonkin aura (balance druid)
+		[ 93435] = true, -- Roar of Courage (cat)
 		[116956] = true, -- Grace of Air (shaman)
 		[128997] = true, -- Spirit Beast Blessing (spirit beast)
+		[155522] = true, -- Power of the Grave (blood death knight)
+		[160073] = true, -- Plainswalking (tallstrider)
+	},
+	multistrike_buff = {
+		[ 24844] = true, -- Breath of the Winds (wind serpent)
+		[ 34889] = true, -- Spry Attacks (dragonhawk)
+		[ 49868] = true, -- Mind Quickening (shadow priest)
+		[ 57386] = true, -- Wild Strength (clefthoof)
+		[ 58604] = true, -- Double Bite (core hound)
+		[109773] = true, -- Dark Intent (warlock)
+		[113742] = true, -- Swiftblade's Cunning (rogue)
+		[166916] = true, -- Windflurry (windwalker monks)
 	},
 	spell_power_multiplier_buff = {
-		[1459] = true, -- Arcane Brillancen (mage)
-		[61316] = true, -- Dalaran Brilliance (mage)
-		[77747] = true, -- Burning Wrath (shaman)
-		[109773] = true,  -- Dark Intent (warlock)
-		[126309] = true, -- Still Water (waterstrider)
+		[  1459] = true, -- Arcane Brilliance (mage)
+		[ 61316] = true, -- Dalaran Brilliance (mage)
+		[ 90364] = true, -- Qiraji Fortitude (silithid)
+		[109773] = true, -- Dark Intent (warlock)
+		[126309] = true, -- Still Water (water strider)
+		[128433] = true, -- Serpent's Cunning (serpent)
 	},
 	stamina_buff = {
-		[469] = true, -- Commanding Shout (warrior)
-		[21562] = true, -- Power Word: Fortitude (priest)
-		[90364] = true, -- Qiraji Fortitude (silithid)
-		[109773] = true,  -- Dark Intent (warlock)
+		[   469] = true, -- Commanding Shout (warrior)
+		[ 21562] = true, -- Power Word: Fortitude (priest)
+		[ 50256] = true, -- Invigorating Roar (bear)
+		[ 90364] = true, -- Qiraji Fortitude (silithid)
+		[160003] = true, -- Savage Vigor (rylak)
+		[160014] = true, -- Sturdiness (goat)
+		[166928] = true, -- Blood Pact (warlock)
 	},
 	str_agi_int_buff = {
-		[1126] = true, -- Mark of the Wild (druid)
-		[20217] = true, -- Blessing of Kings (paladin)
-		[90363] = true, -- Embrace of the Shale Spider (shale spider)
-		[117666] = true, -- Legacy of the Emporer (monk)
+		[  1126] = true, -- Mark of the Wild (druid)
+		[ 20217] = true, -- Blessing of Kings (paladin)
+		[ 90363] = true, -- Embrace of the Shale Spider (shale spider)
+		[115921] = true, -- Legacy of the Emperor (mistweaver monk)
+		[116781] = true, -- Legacy of the White Tiger (brewmaster/windwalker monk)
+		[159988] = true, -- Bark of the Wild (dog, riverbeast)
+		[160017] = true, -- Blessing of Kongs (gorilla)
+		[160077] = true, -- Strength of the Earth (worm)
 	},
-	spell_haste_buff = {
-		[24907] = true, -- Moonkin aura (balance druid)
-		[49868] = true, -- Mind Quickening (shadow priest)
-		[51470] = true, -- Elemental Oath (elemental shaman)
-		[135678] = true, -- Energizing Spores (sporebat)
+	versatility_buff = {
+		[  1126] = true, -- Mark of the Wild (druid)
+		[ 35290] = true, -- Indomitable (boar)
+		[ 50518] = true, -- Chitinous Armor (ravager)
+		[ 55610] = true, -- Unholy Aura (frost/unholy death knight)
+		[ 57386] = true, -- Wild Strength (clefthoof)
+		[159735] = true, -- Tenacity (bird of prey)
+		[160045] = true, -- Defensive Quills (porcupine)
+		[160077] = true, -- Strength of the Earth (worm)
+		[167187] = true, -- Sanctity Aura (retribution paladins)
+		[167188] = true, -- Inspiring Presence (arms/fury warriors)
 	},
 
 	-- Target debuffs
 	bleed_debuff = {
-		[1079] = true, -- Rip (feral druid)
-		[1822] = true, -- Rake (cat druid)
-		[9007] = true, -- Pounce Bleed (cat druid)
-		[33745] = true, -- Lacerate (bear druid)
-		[63468] = true, -- Piercing Shots (marksmanship hunter)
-		[77758] = true, -- Thrash (bear druid)
-		[103830] = true, -- Thrash (cat druid)
+		[  1079] = true, -- Rip (feral druid)
+		[ 16511] = true, -- Hemorrhage (subtlety rogue)
+		[ 33745] = true, -- Lacerate (guardian druid)
+		[ 77758] = true, -- Thrash (druid)
 		[113344] = true, -- Bloodbath (warrior)
 		[115767] = true, -- Deep Wounds (warrior)
-		[120699] = true, -- Lynx Rush (hunter)
 		[122233] = true, -- Crimson Tempest (rogue)
-	},
-	cast_slow_debuff = {
-		[5760] = true, -- Mind-numbing Poison (rogue)
-		[31589] = true, -- Slow (arcane mage)
-		[50274] = true, -- Spore Cloud (sporebat)
-		[58604] = true, -- Lava Breath (core hound)
-		[73975] = true, -- Necrotic Strike (death knight)
-		[90315] = true, -- Tailspin (fox)
-		[109466] = true, -- Curse of Enfeeblement (warlock)
-		[126406] = true, -- Trample (goat)
+		[154953] = true, -- Internal Bleeding (rogue)
+		[155722] = true, -- Rake (cat druid)
 	},
 	healing_reduced_debuff = {
-		[8680] = true, -- Wound Poison (rogue)
-		[54680] = true, -- Monstrous Bite (devilsaur)
-		[82654] = true, -- Widow Venom (hunter)
-		[115804] = true, -- Mortal Wounds (arms & fury warriors, windwalker monk, warlock)
-	},
-	lower_physical_damage_debuff = {
-		[24423] = true, -- Demoralizing Screech (carrion bird)
-		[50256] = true, -- Demoralizing Roar (bear)
-		[115798] = true, -- Weakened Blows (all tank specs, feral druid, retribution paladin, shaman, warlock, warrior)
-	},
-	magic_vulnerability_debuff = {
-		[1490] = true, -- Curse of the Elements (warlock)
-		[24844] = true, -- Lightning Breath (wind serpent)
-		[34889] = true, -- Fire Breath (dragonhawk)
-		[93068] = true, -- Master Poisoner (rogue)
-		[104225] = true, -- Soulburn: Curse of the Elements (warlock)
-		[116202] = true, -- Aura of the Elements (warlock)
-	},
-	physical_vulnerability_debuff = {
-		[35290] = true, -- Gore (boar)
-		[50518] = true, -- Ravage (ravager)
-		[55749] = true, -- Acid Spit (worm)
-		[57386] = true, -- Stampede (rhino)
-		[81326] = true, -- Physical Vulnerability (frost & unholy death knights, retribution paladin, arms & fury warriors)
-	},
-	ranged_vulnerability_debuff = {
-		[1130] = true, -- Hunter's Mark
-	},
-	weakened_armor_debuff = {
-		[113746] = true, -- Weakened Armor
-	},
-	weakened_blows_debuff = {
-		[115798] = true, -- Weakened Blows
+		[  8680] = true, -- Wound Poison (rogue)
+		[ 54680] = true, -- Monstrous Bite (devilsaur)
+		[115804] = true, -- Mortal Wounds (arms/fury warriors, windwalker monk, warlock, carrion bird, scorpid)
 	},
 
 	-- Target buffs
 	enrage_buff = {
-		[12292] = true, -- Bloodbath (warrior)
-		[12880] = true, -- Enrage (warrior)
-		[18499] = true, -- Berserker Rage (warrior)
-		[49016] = true, -- Unholy Frenzy (death knight)
-		[76691] = true, -- Vengeance (all tank specs)
-		[132365] = true, -- Vengeance (all tank specs)
+		[ 12880] = true, -- Enrage (warrior)
+		[ 18499] = true, -- Berserker Rage (warrior)
 	},
 	stealthed_buff = {
-		[1784] = true, -- Stealth
-		[5215] = true, -- Prowl
-		[11327] = true, -- Vanish
-		[24450] = true, -- Prowl (pet)
-		[51713] = true, -- Shadow Dance (subtlety rogue); not truly "stealth" but functions like it for spell usage.
-		[58984] = true, -- Shadowmeld
-		[90328] = true, -- Spirit Walk (spirit beast pet)
+		[  1784] = true, -- Stealth
+		[  5215] = true, -- Prowl
+		[ 11327] = true, -- Vanish
+		[ 24450] = true, -- Prowl (cat)
+		[ 51713] = true, -- Shadow Dance (subtlety rogue); not truly "stealth" but functions like it for spell usage.
+		[ 58984] = true, -- Shadowmeld
+		[ 90328] = true, -- Spirit Walk (spirit beast)
 		[102543] = true, -- Incarnation: King of the Jungle (feral druid); not truly "stealth" but functions like it for spell usage.
-		[113289] = true, -- Prowl (Symbiosis)
-		[115191] = true, -- Stealth (Subterfuge)
-		[115193] = true, -- Vanish (Subterfuge)
-		[115834] = true, -- Shroud of Concealment
 		[148523] = true, -- Jade Mist
+		[115191] = true, -- Stealth (Subterfuge-talented rogue)
+		[115192] = true, -- Subterfuge (rogue); not truly "stealth" but functions like it for spell usage.
+		[115193] = true, -- Vanish (Subterfuge-talented rogue)
 	},
 
 	-- Raid buffs (short term)
 	burst_haste_buff = {
-		[2825] = true, --Bloodlust (Horde shaman)
-		[32182] = true, --Heroism (Alliance shaman)
-		[80353] = true, --Time Warp (mage)
-		[90355] = true, -- Ancient Hysteria (core hound)
-		[146555] = true, -- Drums of Rage
+		[  2825] = true, -- Bloodlust (Horde shaman)
+		[ 32182] = true, -- Heroism (Alliance shaman)
+		[ 80353] = true, -- Time Warp (mage)
+		[ 90355] = true, -- Ancient Hysteria (core hound, nether ray)
 	},
 	burst_haste_debuff = {
-		[57723] = true, -- Exhaustion (Heroism)
-		[57724] = true, -- Sated (Bloodlust)
-		[80354] = true, -- Temporal Displacement (Time Warp)
-		[95809] = true, -- Insanity (Ancient Hysteria)
+		[ 57723] = true, -- Exhaustion (Heroism)
+		[ 57724] = true, -- Sated (Bloodlust)
+		[ 80354] = true, -- Temporal Displacement (Time Warp)
+		[ 95809] = true, -- Insanity (Ancient Hysteria)
 	},
 	raid_movement_buff = {
 		[106898] = true, -- Stampeding Roar
-	},
-	skull_banner_buff = {
-		[114206] = true, -- Skull Banner (warrior)
-	},
-	stormlash_buff = {
-		[120676] = true, -- Stormlash Totem (shaman)
 	},
 }
 -- Deprecated: spell list aliases
@@ -275,10 +222,6 @@ end
 -- Returns true if spellId triggers a fresh snapshot for auraId.
 -- TODO: Handle spreading DoTs (Inferno Blast, etc.) and Soul Swap effects.
 function OvaleData:NeedNewSnapshot(auraId, spellId)
-	-- Don't snapshot if the aura was applied by an action that shouldn't cause the aura to re-snapshot.
-	if self_buffNoSnapshotSpellList[auraId] and self_buffNoSnapshotSpellList[auraId][spellId] then
-		return false
-	end
 	return true
 end
 
