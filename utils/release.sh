e26e9fd5b6d9b95be7b95ef2df1db3b20ef15b5e
Johnny C. Lam <jlam88@gmail.com>
2015-01-18 18:59:17 -0500

    Import latest release.sh from curseforge-packager project.

--- utils/release.sh
+++ utils/release.sh
@@ -25,38 +25,8 @@
 #
 # For more information, please refer to <http://unlicense.org/>
 #
-################################################################################
-#
+
 # release.sh generates a zippable addon directory from a Git checkout.
-#
-# release.sh works by creating a new project directory, checking out external
-# repositories within the project directory, then copying files from the Git
-# checkout into the project directory.  The project directory is then zipped to
-# create a distributable addon zipfile.
-#
-# release.sh reads .pkgmeta and supports the following directives:
-#   - externals
-#   - ignore
-#   - manual-changelog
-#   - package-as
-#
-# release.sh supports the following repository substitution keywords when
-# copying the files from the Git checkout into the project directory.
-#   - @project-version@
-#
-# release.sh reads the TOC file, if present, to determine the name of the
-# project.
-#
-# release.sh assumes that annotated tags are named for the version numbers for
-# the project.  It will identify if the HEAD is tagged and use that as the
-# current version number.  It will search back through parent commits for the
-# previous annotated tag that is a release version number and generate a
-# changelog containing the commits since that previous release tag.
-#
-# By default, release.sh creates releases in a "release" subdirectory of the
-# top-level directory of the Git checkout.
-#
-################################################################################
 
 # POSIX tools.
 cat=cat
@@ -64,6 +34,7 @@ cmp=cmp
 cp=cp
 find=find
 mkdir=mkdir
+mv=mv
 pwd=pwd
 rm=rm
 sed=sed
@@ -80,10 +51,17 @@ zip() {
 	$sevenzip a -tzip $archive "$@"
 }
 
+unix2dos() {
+	$sed -i "s/$/\r/" "$1"
+}
+
 # Variables set via options.
 project=
 topdir=
 releasedir=
+overwrite=
+skip_externals=
+skip_zipfile=
 
 # Set $topdir to top-level directory of the Git checkout.
 if [ -z "$topdir" ]; then
@@ -132,7 +110,7 @@ while getopts ":eon:r:t:z" opt; do
 		;;
 	o)
 		# Skip deleting any previous package directory.
-		skip_delete_pkgdir=true
+		overwrite=true
 		;;
 	r)
 		# Set the release directory to a non-default value.
@@ -175,7 +153,7 @@ $topdir/*)	;;
 	;;
 esac
 
-# Create the release directory.
+# Create the staging directory.
 $mkdir -p "$releasedir"
 
 # Expand $topdir and $releasedir to their absolute paths for string comparisons later.
@@ -206,75 +184,6 @@ else
 	echo "Previous release tag: $rtag"
 fi
 
-# Simple .pkgmeta processor.
-ignore=
-if [ -f "$topdir/.pkgmeta" ]; then
-	while read line; do
-		case $line in
-		package-as:*)
-			phase=${line%%:*}
-			package=${line#*: }
-			pkgdir="$releasedir/$package"
-			if [ -d "$pkgdir" -a -z "$skip_delete_pkgdir" ]; then
-				echo "Removing previous package directory: $pkgdir"
-				$rm -fr "$pkgdir"
-			fi
-			if [ ! -d "$pkgdir" ]; then
-				$mkdir -p "$pkgdir"
-			fi
-			;;
-		externals:*)
-			phase=${line%%:*}
-			;;
-		ignore:*)
-			phase=${line%%:*}
-			;;
-		manual-changelog:*)
-			phase=${line%%:*}
-			changelog=${line#*: }
-			;;
-		filename:*)
-			if [ "$phase" = "manual-changelog" ]; then
-				changelog=${line#*: }
-			fi
-			;;
-		*git*|*svn*)
-			if [ "$phase" = "externals" -a -z "$skip_externals" ]; then
-				dir=${line%%:*}
-				uri=${line#*: }
-				$mkdir -p "$pkgdir/$dir"
-				case $uri in
-				git:*)
-					echo "Getting checkout for $uri"
-					$git clone $uri "$pkgdir/$dir"
-					;;
-				svn:*)
-					echo "Getting checkout for $uri"
-					$svn checkout $uri "$pkgdir/$dir"
-					;;
-				esac
-			fi
-			;;
-		*"- "*)
-			if [ "$phase" = "ignore" ]; then
-				pattern=${line#*- }
-				if [ -d "../$pattern" ]; then
-					pattern="$pattern/*"
-				fi
-				if [ -z "$ignore" ]; then
-					ignore="$pattern"
-				else
-					ignore="$ignore:$pattern"
-				fi
-			fi
-			;;
-		esac
-	done < "$topdir/.pkgmeta"
-	$find "$pkgdir" -name .git -print -o -name .svn -print | while read dir; do
-		$rm -fr "$dir"
-	done
-fi
-
 # Set $version to the version number of HEAD.  May be empty if there are no commits.
 version="$tag"
 if [ -z "$version" ]; then
@@ -282,6 +191,127 @@ if [ -z "$version" ]; then
 	if [ -z "$version" ]; then
 		version=`$git rev-parse --short HEAD 2>/dev/null`
 	fi
+fi
+
+# Variables set via .pkgmeta.
+changelog=
+changelog_markup=text
+enable_nolib_creation="not supported"
+ignore=
+license="LICENSE.txt"
+contents=
+
+### Simple .pkgmeta YAML processor.
+
+yaml_keyvalue() {
+	yaml_key=${1%%:*}
+	yaml_value=${1#$yaml_key:}
+	yaml_value=${yaml_value#"${yaml_value%%[! ]*}"}	# trim leading whitespace
+}
+
+yaml_listitem() {
+	yaml_item=${1#-}
+	yaml_item=${yaml_item#"${yaml_item%%[! ]*}"}	# trim leading whitespace
+}
+
+# Queue for external checkouts.
+external_dir=
+external_uri=
+external_tag=
+
+checkout_queued_external() {
+	if [ -n "$external_dir" -a -n "$external_uri" ]; then
+		$mkdir -p "$pkgdir/$external_dir"
+		echo "Getting checkout for $external_uri"
+		case $external_uri in
+		git:*)
+			$git clone "$external_uri" "$pkgdir/$external_dir"
+			$find "$pkgdir/$external_dir" -name .git -print | while IFS='' read -r dir; do
+				$rm -fr "$dir"
+			done
+			;;
+		svn:*)
+			$svn checkout "$external_uri" "$pkgdir/$external_dir"
+			$find "$pkgdir/$external_dir" -name .svn -print | while IFS='' read -r dir; do
+				$rm -fr "$dir"
+			done
+			;;
+		*)
+			echo "Unknown external: $external_uri" >&2
+			;;
+		esac
+	fi
+	# Clear the queue.
+	external_dir=
+	external_uri=
+	external_tag=
+}
+
+# First scan of .pkgmeta to set variables.
+if [ -f "$topdir/.pkgmeta" ]; then
+	while IFS='' read -r line; do
+		case $line in
+		[!\ ]*:*)
+			# Split $line into a $yaml_key, $yaml_value pair.
+			yaml_keyvalue "$line"
+			# Set the $pkgmeta_phase for stateful processing.
+			pkgmeta_phase=$yaml_key
+
+			case $yaml_key in
+			enable-nolib-creation)
+				enable_nolib_creation=$yaml_value
+				;;
+			license-output)
+				license=$yaml_value
+				;;
+			manual-changelog)
+				changelog=$yaml_value
+				;;
+			package-as)
+				package=$yaml_value
+				;;
+			esac
+			;;
+		" "*)
+			line=${line#"${line%%[! ]*}"}	# trim leading whitespace
+			case $line in
+			"- "*)
+				# Get the YAML list item.
+				yaml_listitem "$line"
+				case $pkgmeta_phase in
+				ignore)
+					pattern=$yaml_item
+					if [ -d "$topdir/$pattern" ]; then
+						pattern="$pattern/*"
+					fi
+					if [ -z "$ignore" ]; then
+						ignore="$pattern"
+					else
+						ignore="$ignore:$pattern"
+					fi
+					;;
+				esac
+				;;
+			*:*)
+				# Split $line into a $yaml_key, $yaml_value pair.
+				yaml_keyvalue "$line"
+				case $pkgmeta_phase in
+				manual-changelog)
+					case $yaml_key in
+					filename)
+						changelog=$yaml_value
+						;;
+					markup-type)
+						changelog_markup=$yaml_value
+						;;
+					esac
+					;;
+				esac
+				;;
+			esac
+			;;
+		esac
+	done < "$topdir/.pkgmeta"
 fi
 
 # Set $package to the basename of the Git checkout directory if not already set.
@@ -298,16 +328,21 @@ if [ -z "$package" ]; then
 fi
 
 # Set $pkgdir to the path of the package directory inside $releasedir.
-if [ -z "$pkgdir" ]; then
-	pkgdir="$releasedir/$package"
-	if [ ! -d "$pkgdir" ]; then
-		$mkdir -p "$pkgdir"
-	fi
+: ${pkgdir:="$releasedir/$package"}
+if [ -d "$pkgdir" -a -z "$overwrite" ]; then
+	echo "Removing previous package directory: $pkgdir"
+	$rm -fr "$pkgdir"
 fi
+if [ ! -d "$pkgdir" ]; then
+	$mkdir -p "$pkgdir"
+fi
+
+# Set the contents of the addon zipfile.
+contents="$package"
 
 # Copy files from working directory into the package directory.
 # Prune away any files in the .git and release directories.
-echo "Copying files into \`\`$pkgdir''..."
+echo "Copying files into \`\`$pkgdir'':"
 $find "$topdir" -name .git -prune -o -name "${releasedir#$topdir/}" -prune -o -print | while read file; do
 	file=${file#$topdir/}
 	if [ "$file" != "$topdir" -a -f "$topdir/$file" ]; then
@@ -372,6 +407,98 @@ $find "$topdir" -name .git -prune -o -name "${releasedir#$topdir/}" -prune -o -p
 	fi
 done
 
+# Create a default license if one doesn't exist.
+if [ -n "$license" -a ! -f "$pkgdir/$license" ]; then
+	echo "All Rights Reserved." > "$pkgdir/$license"
+	unix2dos "$pkgdir/$license"
+fi
+
+# Second scan of .pkgmeta to perform actions.
+if [ -f "$topdir/.pkgmeta" ]; then
+	while IFS='' read -r line; do
+		case $line in
+		[!\ ]*:*)
+			# Started a new section, so checkout any queued externals.
+			checkout_queued_external
+			# Split $line into a $yaml_key, $yaml_value pair.
+			yaml_keyvalue "$line"
+			# Set the $pkgmeta_phase for stateful processing.
+			pkgmeta_phase=$yaml_key
+			;;
+		" "*)
+			line=${line#"${line%%[! ]*}"}	# trim leading whitespace
+			case $line in
+			"- "*)
+				;;
+			*:*)
+				# Split $line into a $yaml_key, $yaml_value pair.
+				yaml_keyvalue "$line"
+				case $pkgmeta_phase in
+				externals)
+					if [ -z "$skip_externals" ]; then
+						case $yaml_key in
+						url)
+							# Queue external URI for checkout.
+							external_uri=$yaml_value
+							;;
+						tag)
+							# Queue external tag for checkout.
+							external_tag=$yaml_value
+							;;
+						*)
+							# Started a new external, so checkout any queued externals.
+							checkout_queued_external
+							external_dir=$yaml_key
+							if [ -n "$yaml_value" ]; then
+								external_uri=$yaml_value
+								# Immediately checkout this fully-specified external.
+								checkout_queued_external
+							fi
+							;;
+						esac
+					fi
+					;;
+				move-folders)
+					srcdir="$releasedir/$yaml_key"
+					destdir="$releasedir/$yaml_value"
+					if [ -d "$destdir" -a -z "$overwrite" ]; then
+						echo "Removing previous moved folder: $destdir"
+						$rm -fr "$destdir"
+					fi
+					if [ -d "$srcdir" ]; then
+						if [ -z "$overwrite" ]; then
+							echo "Moving \`\`$yaml_key'' to \`\`$destdir''"
+							$mv "$srcdir" "$destdir"
+						else
+							echo "Copying contents of \`\`$yaml_key'' to \`\`$destdir''"
+							$mkdir -p "$destdir"
+							$find "$srcdir" -print | while read file; do
+								file=${file#"$releasedir/"}
+								if [ "$file" != "$releasedir" -a -f "$releasedir/$file" ]; then
+									dir=${file%/*}
+									if [ "$dir" != "$file" ]; then
+										$mkdir -p "$destdir/$dir"
+									fi
+									$cp "$releasedir/$file" "$destdir/$dir"
+									echo "Copied: $file"
+								fi
+							done
+						fi
+						contents="$contents $yaml_value"
+						# Copy the license into $destdir if one doesn't already exist.
+						if [ ! -f "$destdir/$license" ]; then
+							$cp -f "$pkgdir/$license" "$destdir/$license"
+						fi
+					fi
+					;;
+				esac
+				;;
+			esac
+			;;
+		esac
+	done < "$topdir/.pkgmeta"
+fi
+
 # Find the name of the project if unset.
 if [ -z "$project" ]; then
 	# Parse the TOC file if it exists for the title of the project.
@@ -414,7 +541,7 @@ $change_string
 
 EOF
 	$git log $git_commit_range --pretty=format:"- %B" >> "$pkgdir/$changelog"
-	$sed -i "s/$/\r/" "$pkgdir/$changelog"
+	unix2dos "$pkgdir/$changelog"
 fi
 
 # Creating the final zipfile for the addon.
@@ -428,5 +555,5 @@ if [ -z "$skip_zipfile" ]; then
 		echo "Removing previous archive: $archive"
 		$rm -f "$archive"
 	fi
-	$zip "$archive" "$pkgdir"
+	( cd "$releasedir" && $zip "$archive" $contents )
 fi
