2cb1a62122ae9e65d32d072d9b5ab0831fdb9d30
Johnny C. Lam <jlam88@gmail.com>
2015-03-10 03:17:07 -0400

    Improvements to OvaleGUID module.
    - Remove a small memory leak.
    
    - Rename methods to match the names or to follow the naming pattern of the
      Blizzard API functions.
    
    - Allow methods to return multiple values if the result is a one to many
      mapping, e.g., GUID to unit IDs.
    
    - Raise an Ovale_PetChanged event with the GUID of the player's pet.

--- GUID.lua
+++ GUID.lua
@@ -1,19 +1,21 @@
 --[[--------------------------------------------------------------------
     Copyright (C) 2012 Sidoine De Wispelaere.
-    Copyright (C) 2012, 2013 Johnny C. Lam.
+    Copyright (C) 2012, 2013, 2015 Johnny C. Lam.
     See the file LICENSE.txt for copying permission.
 --]]--------------------------------------------------------------------
 
 --[[
-	This addon manages mappings between GUID <--> unit ID <--> unit name.
+	This addon manages mappings between GUIDs, unit IDs, and names.
 
-	A GUID can have multiple unit IDs.
 	A unit ID can only have one GUID.
+	A unit ID can only have one name.
 	A unit ID may not exist.
 
-	All primary unit IDs receive events.
-	No <unit>target IDs receive events.
-	No "mouseover" unit IDs receive events.
+	A GUID can have multiple unit IDs.
+	A GUID can only have one name.
+
+	A name can have multiple unit IDs.
+	A name can have mulitple GUIDs.
 --]]
 
 local OVALE, Ovale = ...
@@ -21,57 +23,20 @@ local OvaleGUID = Ovale:NewModule("OvaleGUID", "AceEvent-3.0")
 Ovale.OvaleGUID = OvaleGUID
 
 --<private-static-properties>
-local L = Ovale.L
 local OvaleDebug = Ovale.OvaleDebug
 
+local floor = math.floor
 local ipairs = ipairs
-local next = next
-local pairs = pairs
+local setmetatable = setmetatable
 local tinsert = table.insert
-local API_GetNumGroupMembers = GetNumGroupMembers
+local tremove = table.remove
+local type = type
+local unpack = unpack
 local API_UnitGUID = UnitGUID
 local API_UnitName = UnitName
 
 -- Register for debugging messages.
 OvaleDebug:RegisterDebugging(OvaleGUID)
-
---[[
-	Unit IDs for which UNIT_AURA events are known to fire.
-
-	UNIT_AURA_UNITS is an ordered list of unit ID priority.
-	UNIT_AURA_UNIT is a table that holds the reverse mapping of UNIT_AURA_UNITS.
---]]
-local UNIT_AURA_UNITS = {}
-do
-	tinsert(UNIT_AURA_UNITS, "player")
-	tinsert(UNIT_AURA_UNITS, "pet")
-	tinsert(UNIT_AURA_UNITS, "vehicle")
-	tinsert(UNIT_AURA_UNITS, "npc")
-	tinsert(UNIT_AURA_UNITS, "target")
-	tinsert(UNIT_AURA_UNITS, "focus")
-	for i = 1, 5 do
-		tinsert(UNIT_AURA_UNITS, "arena" .. i)
-		tinsert(UNIT_AURA_UNITS, "arenapet" .. i)
-	end
-	for i = 1, 40 do
-		tinsert(UNIT_AURA_UNITS, "raid" .. i)
-		tinsert(UNIT_AURA_UNITS, "raidpet" .. i)
-	end
-	for i = 1, 4 do
-		tinsert(UNIT_AURA_UNITS, "party" .. i)
-		tinsert(UNIT_AURA_UNITS, "partypet" .. i)
-	end
-	for i = 1, 4 do
-		tinsert(UNIT_AURA_UNITS, "boss" .. i)
-	end
-end
-
-local UNIT_AURA_UNIT = {}
-do
-	for i, unitId in ipairs(UNIT_AURA_UNITS) do
-		UNIT_AURA_UNIT[unitId] = i
-	end
-end
 
 -- PET_UNIT[unitId] = pet's unit ID
 local PET_UNIT = {}
@@ -86,31 +51,138 @@ do
 	for i = 1, 40 do
 		PET_UNIT["raid" .. i] = "raidpet" .. i
 	end
+	-- Default unit pet name is the unit ID with "pet" appended to it.
+	setmetatable(PET_UNIT, { __index = function(t, unitId) return unitId .. "pet" end })
+end
+
+--[[
+	Unit IDs for which UNIT_AURA events are known to fire.
+
+	UNIT_AURA_UNITS is an ordered list of unit ID priority.
+	UNIT_AURA_UNIT is a table that holds the reverse mapping of UNIT_AURA_UNITS.
+--]]
+local UNIT_AURA_UNITS = {}
+do
+	tinsert(UNIT_AURA_UNITS, "player")
+	tinsert(UNIT_AURA_UNITS, "pet")
+	tinsert(UNIT_AURA_UNITS, "vehicle")
+	tinsert(UNIT_AURA_UNITS, "target")
+	tinsert(UNIT_AURA_UNITS, "focus")
+	for i = 1, 40 do
+		local unitId = "raid" .. i
+		tinsert(UNIT_AURA_UNITS, unitId)
+		tinsert(UNIT_AURA_UNITS, PET_UNIT[unitId])
+	end
+	for i = 1, 4 do
+		local unitId = "party" .. i
+		tinsert(UNIT_AURA_UNITS, unitId)
+		tinsert(UNIT_AURA_UNITS, PET_UNIT[unitId])
+	end
+	for i = 1, 4 do
+		tinsert(UNIT_AURA_UNITS, "boss" .. i)
+	end
+	for i = 1, 5 do
+		local unitId = "arena" .. i
+		tinsert(UNIT_AURA_UNITS, unitId)
+		tinsert(UNIT_AURA_UNITS, PET_UNIT[unitId])
+	end
+	tinsert(UNIT_AURA_UNITS, "npc")
+end
+
+local UNIT_AURA_UNIT = {}
+do
+	for i, unitId in ipairs(UNIT_AURA_UNITS) do
+		UNIT_AURA_UNIT[unitId] = i
+	end
+	-- Default unit priority is after all listed units.
+	setmetatable(UNIT_AURA_UNIT, { __index = function(t, unitId) return #t + 1 end })
 end
 --</private-static-properties>
 
 --<public-static-properties>
---[[
-	Unit ID --> GUID mapping.
-	unit ID can only have one GUID.
-	self.unitIdToGUID[unitId] = GUID
---]]
-OvaleGUID.unitIdToGUID = {}
+-- Mappings between GUIDs, unit IDS, and names.
+OvaleGUID.unitGUID = {}
+OvaleGUID.guidUnit = {}
+OvaleGUID.unitName = {}
+OvaleGUID.nameUnit = {}
+OvaleGUID.guidName = {}
+OvaleGUID.nameGUID = {}
 
---[[
-	GUID --> unit ID mapping.
-	A GUID can have multiple unit IDs.
-	self.GUIDtoUnitId[guid] = { unitId = true if it exists and points to guid; nil otherwise }
---]]
-OvaleGUID.GUIDtoUnitId = {}
-
---[[
---]]
+-- Deprecated: legacy mapping from name to GUID.
 OvaleGUID.nameToGUID = {}
 
 -- Export UNIT_AURA_UNIT table of units that receive UNIT_AURA events.
 OvaleGUID.UNIT_AURA_UNIT = UNIT_AURA_UNIT
 --</public-static-properties>
+
+--<private-static-methods>
+local BinaryInsert
+local BinaryRemove
+local BinarySearch
+do
+	-- Binary search algorithm pseudocode from: http://rosettacode.org/wiki/Binary_search
+
+	local function compareDefault(a, b)
+		return a < b
+	end
+
+	-- Insert the value at the rightmost insertion point of a sorted array using binary search.
+	BinaryInsert = function(t, value, unique, compare)
+		if type(unique) == "function" then
+			unique, compare = nil, unique
+		end
+		compare = compare or compareDefault
+		local low, high = 1, #t
+		while low <= high do
+			-- invariants: value >= t[i] for all i < low
+			--             value < t[i] for all i > high
+			local mid = floor((low + high) / 2)
+			if compare(value, t[mid]) then
+				high = mid - 1
+			elseif not unique or compare(t[mid], value) then
+				low = mid + 1
+			else
+				return mid
+			end
+		end
+		tinsert(t, low, value)
+		return low
+	end
+
+	-- Remove the value in a sorted array using binary search.
+	BinaryRemove = function(t, value, compare)
+		local index = BinarySearch(t, value, compare)
+		if index then
+			tremove(t, index)
+		end
+		return index
+	end
+
+	-- Return the index of the value in a sorted array using binary search.
+	BinarySearch = function(t, value, compare)
+		compare = compare or compareDefault
+		local low, high = 1, #t
+		while low <= high do
+			-- invariants: value > t[i] for all i < low
+			--             value < t[i] for all i > high
+			local mid = floor((low + high) / 2)
+			if compare(value, t[mid]) then
+				high = mid - 1
+			elseif compare(t[mid], value) then
+				low = mid + 1
+			else
+				return mid
+			end
+		end
+		return nil
+	end
+end
+
+-- Comparator for unit IDs based on their unit priorities from UNIT_AURA_UNIT.
+local function CompareUnit(a, b)
+	return UNIT_AURA_UNIT[a] < UNIT_AURA_UNIT[b]
+end
+--</private-static-methods>
 
 --<public-static-methods>
 function OvaleGUID:OnEnable()
@@ -120,11 +192,9 @@ function OvaleGUID:OnEnable()
 	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateAllUnits")
 	self:RegisterEvent("PLAYER_FOCUS_CHANGED")
 	self:RegisterEvent("PLAYER_TARGET_CHANGED")
+	self:RegisterEvent("UNIT_NAME_UPDATE")
 	self:RegisterEvent("UNIT_PET")
 	self:RegisterEvent("UNIT_TARGET")
-	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-
-	self:UpdateAllUnits()
 end
 
 function OvaleGUID:OnDisable()
@@ -135,57 +205,148 @@ function OvaleGUID:OnDisable()
 	self:UnregisterEvent("PLAYER_FOCUS_CHANGED")
 	self:UnregisterEvent("PLAYER_LOGIN")
 	self:UnregisterEvent("PLAYER_TARGET_CHANGED")
+	self:UnregisterEvent("UNIT_NAME_UPDATE")
 	self:UnregisterEvent("UNIT_PET")
 	self:UnregisterEvent("UNIT_TARGET")
-	self:UnregisterEvent("UPDATE_MOUSEOVER_UNIT")
 end
 
 function OvaleGUID:ARENA_OPPONENT_UPDATE(event, unitId, eventType)
-	for i = 1, 5 do
-		local unit = "arena" .. i
-		self:UpdateUnitWithTarget(unit)
-		local pet = PET_UNIT[unit] or (unit .. "pet")
-		self:UpdateUnitWithTarget(pet)
+	if eventType ~= "cleared" or self.unitGUID[unitId] then
+		self:Debug(event, unitId, eventType)
+		self:UpdateUnitWithTarget(unitId)
 	end
 end
 
 function OvaleGUID:GROUP_ROSTER_UPDATE(event)
+	self:Debug(event)
 	self:UpdateAllUnits()
 	self:SendMessage("Ovale_GroupChanged")
 end
 
 function OvaleGUID:INSTANCE_ENCOUNTER_ENGAGE_UNIT(event)
+	self:Debug(event)
 	for i= 1, 4 do
 		self:UpdateUnitWithTarget("boss" .. i)
 	end
 end
 
 function OvaleGUID:PLAYER_FOCUS_CHANGED(event)
+	self:Debug(event)
 	self:UpdateUnitWithTarget("focus")
 end
 
 function OvaleGUID:PLAYER_TARGET_CHANGED(event, cause)
-	self:UNIT_TARGET(event, "player")
+	self:Debug(event, cause)
+	self:UpdateUnit("target")
+end
+
+function OvaleGUID:UNIT_NAME_UPDATE(event, unitId)
+	self:Debug(event, unitId)
+	self:UpdateUnit(unitId)
 end
 
 function OvaleGUID:UNIT_PET(event, unitId)
-	local pet = PET_UNIT[unitId] or (unitId .. "pet")
+	self:Debug(event, unitId)
+	local pet = PET_UNIT[unitId]
 	self:UpdateUnitWithTarget(pet)
+	if unitId == "player" then
+		local petGUID = self:UnitGUID("pet")
+		self:SendMessage("Ovale_PetChanged", petGUID)
+	end
 	self:SendMessage("Ovale_GroupChanged")
 end
 
 function OvaleGUID:UNIT_TARGET(event, unitId)
-	local target = (unitId == "player") and "target" or (unitId .. "target")
-	self:UpdateUnit(target)
-end
-
-function OvaleGUID:UPDATE_MOUSEOVER_UNIT(event)
-	self:UpdateUnitWithTarget("mouseover")
+	-- Changes to the player's target are tracked with PLAYER_TARGET_CHANGED.
+	if unitId ~= "player" then
+		self:Debug(event, unitId)
+		local target = unitId .. "target"
+		self:UpdateUnit(target)
+	end
 end
 
 function OvaleGUID:UpdateAllUnits()
-	for _, unitId in pairs(UNIT_AURA_UNITS) do
+	for _, unitId in ipairs(UNIT_AURA_UNITS) do
 		self:UpdateUnitWithTarget(unitId)
+	end
+end
+
+function OvaleGUID:UpdateUnit(unitId)
+	local guid = API_UnitGUID(unitId)
+	local name = API_UnitName(unitId)
+	local previousGUID = self.unitGUID[unitId]
+	local previousName = self.unitName[unitId]
+	--[[
+		Remove the previous GUID and name mappings for this unit ID if they've changed.
+	--]]
+	if not guid or guid ~= previousGUID then
+		-- unit <--> GUID
+		self.unitGUID[unitId] = nil
+		if previousGUID then
+			if self.guidUnit[previousGUID] then
+				BinaryRemove(self.guidUnit[previousGUID], unitId, CompareUnit)
+			end
+			Ovale.refreshNeeded[previousGUID] = true
+		end
+	end
+	if not name or name ~= previousName then
+		-- unit <--> name
+		self.unitName[unitId] = nil
+		if previousName and self.nameUnit[previousName] then
+			BinaryRemove(self.nameUnit[previousName], unitId, CompareUnit)
+		end
+	end
+	if guid and guid == previousGUID and name and name ~= previousName then
+		-- GUID <--> name
+		self.guidName[guid] = nil
+		if previousName and self.nameGUID[previousName] then
+			BinaryRemove(self.nameGUID[previousName], guid, CompareUnit)
+		end
+	end
+	--[[
+		Create new mappings from this unit ID to the current GUID and name.
+	--]]
+	if guid and guid ~= previousGUID then
+		-- unit <--> GUID
+		self.unitGUID[unitId] = guid
+		do
+			local list = self.guidUnit[guid] or {}
+			BinaryInsert(list, unitId, true, CompareUnit)
+			self.guidUnit[guid] = list
+		end
+		self:Debug("'%s' is '%s'.", unitId, guid)
+		Ovale.refreshNeeded[guid] = true
+	end
+	if name and name ~= previousName then
+		-- unit <--> name
+		self.unitName[unitId] = name
+		do
+			local list = self.nameUnit[name] or {}
+			BinaryInsert(list, unitId, true, CompareUnit)
+			self.nameUnit[name] = list
+		end
+		self:Debug("'%s' is '%s'.", unitId, name)
+	end
+	if guid and name then
+		-- GUID <--> name
+		local previousNameFromGUID = self.guidName[guid]
+		self.guidName[guid] = name
+		if name ~= previousNameFromGUID then
+			local list = self.nameGUID[name] or {}
+			BinaryInsert(list, guid, true)
+			self.nameGUID[name] = list
+			if guid == previousGUID then
+				self:Debug("'%s' changed names to '%s'.", guid, name)
+			else
+				self:Debug("'%s' is '%s'.", guid, name)
+			end
+		end
+	end
+	-- Deprecated: legacy mapping from name to GUID.
+	if not self.nameToGUID[name] then
+		if guid and guid ~= previousGUID and name and (unitId == "target" or self.unitGUID.target ~= guid) then
+			self.nameToGUID[name] = guid
+		end
 	end
 end
 
@@ -194,84 +355,70 @@ function OvaleGUID:UpdateUnitWithTarget(unitId)
 	self:UpdateUnit(unitId .. "target")
 end
 
-function OvaleGUID:UpdateUnit(unitId)
-	local guid = API_UnitGUID(unitId)
-	if guid then
-		local previousGUID = self.unitIdToGUID[unitId]
-		if previousGUID ~= guid then
-			-- Remove previous mappings for this unit ID.
-			if previousGUID and self.GUIDtoUnitId[previousGUID] then
-				self.GUIDtoUnitId[previousGUID][unitId] = nil
-				if not next(self.GUIDtoUnitId[previousGUID]) then
-					self.GUIDtoUnitId[previousGUID] = nil
-				end
-			end
-			-- Create new mappings this unit ID to the GUID.
-			self.unitIdToGUID[unitId] = guid
-			self.GUIDtoUnitId[guid] = self.GUIDtoUnitId[guid] or {}
-			self.GUIDtoUnitId[guid][unitId] = true
-
-			self:Debug("'%s' is GUID %s.", unitId, guid)
-
-			if unitId == "target" or self.unitIdToGUID.target ~= guid then
-				local name = API_UnitName(unitId)
-				self.nameToGUID[name] = self.nameToGUID[name] or guid
-			end
-			Ovale.refreshNeeded[guid] = true
-		end
-	else
-		-- This unit ID doesn't point to a valid GUID.
-		self.unitIdToGUID[unitId] = nil
-		if self.GUIDtoUnitId[guid] then
-			self.GUIDtoUnitId[guid][unitId] = nil
-			if not next(self.GUIDtoUnitId[guid]) then
-				self.GUIDtoUnitId[guid] = nil
-			end
-		end
-		Ovale.refreshNeeded[guid] = true
-	end
-end
-
-function OvaleGUID:GetGUID(unitId)
+-- Return the GUID of the given unit.
+function OvaleGUID:UnitGUID(unitId)
 	if unitId then
-		-- If the unit ID doesn't receive events, then refresh it now.
-		if not UNIT_AURA_UNIT[unitId] then
-			self:UpdateUnit(unitId)
-		end
-		return self.unitIdToGUID[unitId]
+		return self.unitGUID[unitId] or API_UnitGUID(unitId)
 	end
 	return nil
 end
 
-function OvaleGUID:GetUnitId(guid)
-	if self.GUIDtoUnitId[guid] then
-		-- Find the unit ID with the best (lowest) priority.
-		local bestUnitId, bestPriority
-		for unitId in pairs(self.GUIDtoUnitId[guid]) do
-			local priority = UNIT_AURA_UNIT[unitId]
-			if priority then
-				if not bestPriority or priority < bestPriority then
-					bestUnitId, bestPriority = unitId, priority
-				end
-			else
-				-- This isn't a unit ID that receives events, so refresh it to make
-				-- sure it still points to this GUID.
-				self:UpdateUnit(unitId)
-				if not bestPriority and self.unitIdToGUID[unitId] == guid then
-					bestUnitId = unitId
-				end
-			end
-		end
-		return bestUnitId
+-- Return a list of unit IDs for the given GUID.
+function OvaleGUID:GUIDUnit(guid)
+	if guid and self.guidUnit[guid] then
+		return unpack(self.guidUnit[guid])
 	end
 	return nil
 end
+
+-- Return the name of the given unit.
+function OvaleGUID:UnitName(unitId)
+	if unitId then
+		return self.unitName[unitId] or API_UnitName(unitId)
+	end
+	return nil
+end
+
+-- Return a list of the unit IDs with the given name.
+function OvaleGUID:NameUnit(name)
+	if name and self.nameUnit[name] then
+		return unpack(self.nameUnit[name])
+	end
+	return nil
+end
+
+-- Return the name of the given GUID.
+function OvaleGUID:GUIDName(guid)
+	if guid then
+		return self.guidName[guid]
+	end
+	return nil
+end
+
+-- Return a list of the GUIDs with the given name.
+function OvaleGUID:NameGUID(name)
+	if name and self.nameGUID[name] then
+		return unpack(self.nameGUID[name])
+	end
+	return nil
+end
+
+--[[
+	Deprecated: old method names.
+
+	function OvaleGUID:GetGUID(unitId) end
+	function OvaleGUID:GetUnitId(guid) end
+	function OvaleGUID:GetGUIDForName(name) end
+	function OvaleGUID:GetUnitIdForName(name) end
+--]]
+OvaleGUID.GetGUID = OvaleGUID.UnitGUID
+OvaleGUID.GetUnitId = OvaleGUID.GUIDUnit
+OvaleGUID.GetUnitIdForName = OvaleGUID.NameUnit
 
 function OvaleGUID:GetGUIDForName(name)
-	return self.nameToGUID[name]
-end
-
-function OvaleGUID:GetUnitIdForName(name)
-	return self:GetUnitId(self:GetGUIDForName(name))
+	if name then
+		return self.nameToGUID[name]
+	end
+	return nil
 end
 --</public-static-methods>
