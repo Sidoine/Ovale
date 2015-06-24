e9af703f5aeb4fe2839363172848f4bbf6609a87
Johnny C. Lam <jlam88@gmail.com>
2015-06-23 21:27:40 -0400

    Fix logging message for the stance requirement handler.

--- Stance.lua
+++ Stance.lua
@@ -259,9 +259,9 @@ function OvaleStance:RequireStanceHandler(spellId, atTime, requirement, tokens, 
 		end
 		local result = verified and "passed" or "FAILED"
 		if isBang then
-			self:Log("    Require stance '%s': %s", stance, result)
+			self:Log("    Require NOT stance '%s': %s", stance, result)
 		else
-			self:Log("    Require NOT stance 's': %s", stance, result)
+			self:Log("    Require stance '%s': %s", stance, result)
 		end
 	else
 		Ovale:OneTimeMessage("Warning: requirement '%s' is missing a stance argument.", requirement)
