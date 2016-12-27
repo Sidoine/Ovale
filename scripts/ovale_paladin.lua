c9dcea2f649ff4dd9430fa2a88073f560bf18e6e
Kevin De Smet <kevin.de.smet@outlook.be>
2016-12-27 23:31:47 +0100

    checking for blessed hammer debuff is too unpredictable

--- scripts/ovale_paladin.lua
+++ scripts/ovale_paladin.lua
@@ -16,7 +16,7 @@ AddCheckBox(opt_legendary_ring_tank ItemName(legendary_ring_bonus_armor) default
 
 AddFunction ProtectionDefaultMainActions
 {
-	if Talent(blessed_hammer_talent) and (target.BuffExpires(blessed_hammer_debuff) or SpellCharges(blessed_hammer) == SpellMaxCharges(blessed_hammer)) Spell(blessed_hammer)
+	if Talent(blessed_hammer_talent) Spell(blessed_hammer)
 	Spell(judgment)
 	if Talent(crusaders_judgment_talent) and BuffPresent(grand_crusader_buff) Spell(avengers_shield)
 	if not Talent(consecrated_hammer_talent) and not BuffPresent(consecration_buff) Spell(consecration)
@@ -30,7 +30,7 @@ AddFunction ProtectionDefaultAoEActions
 {
 	Spell(avengers_shield)
 	if not Talent(consecrated_hammer_talent) and not BuffPresent(consecration_buff) Spell(consecration)
-	if Talent(blessed_hammer_talent) and (target.BuffExpires(blessed_hammer_debuff) or SpellCharges(blessed_hammer) == SpellMaxCharges(blessed_hammer)) Spell(blessed_hammer)
+	if Talent(blessed_hammer_talent) Spell(blessed_hammer)
 	Spell(judgment)
 	Spell(blinding_light)
 	Spell(consecration)
