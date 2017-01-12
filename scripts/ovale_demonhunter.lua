5898eec291d3ce28430f9e78ccf2bd1b4a414eeb
Kevin De Smet <kevin.de.smet@outlook.be>
2017-01-13 00:22:16 +0100

    Razor spikes

--- scripts/ovale_demonhunter.lua
+++ scripts/ovale_demonhunter.lua
@@ -17,7 +17,7 @@ AddCheckBox(opt_melee_range L(not_in_melee_range) specialization=vengeance)
 
 AddFunction VengeancePlayDefensively
 {
-	CheckBoxOn(opt_demonhunter_vengeance_defensive)
+	CheckBoxOn(opt_demonhunter_vengeance_defensive) or HealthPercent() <= 35 or IncomingDamage(5) >= MaxHealth() * 0.5
 }
 
 AddFunction VengeancePlayOffensively
@@ -50,6 +50,8 @@ AddFunction VengeanceDefaultShortCDActions
 AddFunction VengeanceDefaultMainActions
 {
 	VengeanceHealMe()
+	
+	if (VengeancePlayOffensively() and Talent(razor_spikes_talent) and not BuffExpires(demon_spikes_buff)) Spell(fracture)
 	if (VengeancePlayOffensively() and HasArtifactTrait(fiery_demise) and target.TimeToDie() >= 8) Spell(fiery_brand)
 	if (VengeancePlayOffensively() or BuffStacks(soul_fragments) <= 2) Spell(soul_carver)
 	if (VengeancePlayOffensively()) Spell(fel_devastation)
@@ -71,6 +73,8 @@ AddFunction VengeanceDefaultMainActions
 AddFunction VengeanceDefaultAoEActions
 {
 	VengeanceHealMe()
+	
+	if (VengeancePlayOffensively() and Talent(razor_spikes_talent) and not BuffExpires(demon_spikes_buff)) Spell(soul_cleave)
 	if (BuffStacks(soul_fragments) <= 2) Spell(soul_carver)
 	if (VengeancePlayOffensively()) Spell(fel_devastation)
 	if (Pain() >= SpellData(demon_spikes pain) + SpellData(soul_cleave pain) + SpellData(soul_cleave extra_pain)) Spell(soul_cleave)
