df5f23fe8cb70f370b71ad083e92af1241ab0923
Kevin De Smet <kevin.de.smet@outlook.be>
2017-05-15 13:39:58 +0200

    use fiery brand more freely

--- scripts/ovale_demonhunter.lua
+++ scripts/ovale_demonhunter.lua
@@ -77,8 +77,12 @@ AddFunction VengeanceDefaultMainActions
 	}
 	
 	# Fiery demise
-	if (HasArtifactTrait(fiery_demise) and VengeancePlayOffensively() and SpellCooldown(soul_carver) <= 6.5 and (not Talent(fel_devastation_talent) or (SpellCooldown(fel_devastation) <= 4 and Pain() >= 30)) and target.TimeToDie() >= 8) Spell(fiery_brand)
-	if (HasArtifactTrait(fiery_demise) and VengeancePlayOffensively() and not target.BuffExpires(fiery_demise_debuff))
+	if (HasArtifactTrait(fiery_demise) and VengeancePlayOffensively() 
+		and (SpellCooldown(soul_carver) <= 6.5 or SpellCooldown(soul_carver) > SpellCooldownDuration(fiery_brand)) 
+		and (not Talent(fel_devastation_talent) or (SpellCooldown(fel_devastation) <= 4 and Pain() >= 30) or SpellCooldown(fel_devastation) > SpellCooldownDuration(fiery_brand)) 
+		and target.TimeToDie() >= 8
+	) Spell(fiery_brand)
+	if (VengeancePlayOffensively() and not target.BuffExpires(fiery_demise_debuff))
 	{
 		if (PainDeficit() > 10) Spell(immolation_aura)
 		Spell(soul_carver)
@@ -90,8 +94,8 @@ AddFunction VengeanceDefaultMainActions
 	}
 	
 	# Regular rotation
-	if (BuffStacks(soul_fragments) <= 2) Spell(soul_carver)
-	if ((VengeancePlayOffensively() and not HasArtifactTrait(fiery_demise))) Spell(fel_devastation)
+	if (VengeancePlayDefensively() and BuffStacks(soul_fragments) <= 2) Spell(soul_carver)
+	if (VengeancePlayOffensively() and not HasArtifactTrait(fiery_demise)) Spell(fel_devastation)
 	if (Pain() >= 80 and not Talent(fracture_talent)) Spell(soul_cleave)
 	if (PainDeficit() > 10) Spell(immolation_aura)
 	if (PainDeficit() > 20) Spell(felblade)
