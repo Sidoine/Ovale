7fdfc721267e5eab0ce9260ab521c9584e004ea5
Kevin De Smet <kevin.de.smet@outlook.be>
2017-06-14 14:11:04 +0200

    max duration of ISB is 3*BaseDuration

--- scripts/ovale_monk.lua
+++ scripts/ovale_monk.lua
@@ -46,28 +46,32 @@ AddFunction BrewmasterDefaultShortCDActions
 	# range check
 	BrewmasterRangeCheck()
 
-	unless StaggerPercentage() > 100 or BrewmasterHealMe() or (StaggerRemaining() == 0 and not Talent(special_delivery_talent))
+	unless StaggerPercentage() > 100 or BrewmasterHealMe()
 	{
 		# purify heavy stagger when we have enough ISB
-		if (DebuffPresent(heavy_stagger_debuff) and (not Talent(elusive_dance_talent) or BuffExpires(elusive_dance_buff)) and (BuffRemaining(ironskin_brew_buff) > 1.5*SpellRechargeDuration(ironskin_brew))) Spell(purifying_brew)
+		if (DebuffPresent(heavy_stagger_debuff) and (BuffRemaining(ironskin_brew_buff) > 2*BaseDuration(ironskin_brew))) Spell(purifying_brew)
 		# purify medium stagger when low on hp
 		if ((DebuffPresent(heavy_stagger_debuff) or DebuffPresent(moderate_stagger_debuff)) and HealthPercent() <= 50) Spell(purifying_brew)
 		# always keep 1 charge unless black_ox_brew is coming off cd
-		if (SpellCharges(ironskin_brew) > 1 or (Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 3))
+		if (SpellCharges(ironskin_brew count=0) >= 1.9 or (Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 3))
 		{
 			# never be at (almost) max charges 
-			if (SpellCharges(ironskin_brew count=0) >= SpellMaxCharges(ironskin_brew)-0.3) Spell(ironskin_brew)
+			if (SpellCharges(ironskin_brew count=0) >= SpellMaxCharges(ironskin_brew)-0.3)
+			{
+				if (BuffRemaining(ironskin_brew_buff) <= 2*BaseDuration(ironskin_brew)) Spell(ironskin_brew)
+				Spell(purifying_brew)
+			}
 			# use up those charges when black_ox_brew_talent comes off cd
 			if (Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 3)
 			{
-				if (DebuffPresent(heavy_stagger_debuff) or DebuffPresent(moderate_stagger_debuff)) Spell(purifying_brew)
+				Spell(purifying_brew)
 				Spell(ironskin_brew)
 			}
 			
 			if(StaggerRemaining() > 0)
 			{
 				# keep brew-stache rolling (when not elusive_dance)
-				if (HasArtifactTrait(brew_stache_trait) and BuffExpires(brew_stache_buff) and not Talent(elusive_dance_talent)) Spell(ironskin_brew text=stache)
+				if (HasArtifactTrait(brew_stache_trait) and BuffExpires(brew_stache_buff) and not Talent(elusive_dance_talent) and (BuffRemaining(ironskin_brew_buff) <= 2*BaseDuration(ironskin_brew))) Spell(ironskin_brew text=stache)
 				# keep up ironskin_brew_buff
 				if (BuffExpires(ironskin_brew_buff 2)) Spell(ironskin_brew)
 				# purify stagger when talent elusive dance 
