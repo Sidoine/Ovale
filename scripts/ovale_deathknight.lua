34048be491272098e557b922df10ce2f5643bd17
Kevin De Smet <kevin.de.smet@outlook.be>
2017-08-04 22:43:01 +0200

    remove soulgorge talent

--- scripts/ovale_deathknight.lua
+++ scripts/ovale_deathknight.lua
@@ -25,8 +25,7 @@ AddFunction BloodDefaultMainActions
 {
 	BloodHealMe()
 	if InCombat() and BuffExpires(bone_shield_buff 3) Spell(marrowrend)
-	if not Talent(soulgorge_talent) and target.DebuffRefreshable(blood_plague_debuff) Spell(blood_boil)
-	if target.DebuffRefreshable(blood_plague_debuff) Spell(deaths_caress)
+	if target.DebuffRefreshable(blood_plague_debuff) Spell(blood_boil)
 	if not BuffPresent(death_and_decay_buff) and BuffPresent(crimson_scourge_buff) and Talent(rapid_decomposition_talent) Spell(death_and_decay)
 	if RunicPower() >= 100 and target.TimeToDie() >= 10 Spell(bonestorm)
 	if RunicPowerDeficit() <= 20 Spell(death_strike)
@@ -43,7 +42,7 @@ AddFunction BloodDefaultAoEActions
 	BloodHealMe()
 	if RunicPower() >= 100 Spell(bonestorm)
 	if InCombat() and BuffExpires(bone_shield_buff 3) Spell(marrowrend)
-	if not Talent(soulgorge_talent) and DebuffCountOnAny(blood_plague_debuff) < Enemies(tagged=1) Spell(blood_boil)
+	if DebuffCountOnAny(blood_plague_debuff) < Enemies(tagged=1) Spell(blood_boil)
 	if not BuffPresent(death_and_decay_buff) and BuffPresent(crimson_scourge_buff) Spell(death_and_decay)
 	if RunicPowerDeficit() <= 20 Spell(death_strike)
 	if BuffStacks(bone_shield_buff) <= 2+4*Talent(ossuary_talent) Spell(marrowrend)
@@ -52,7 +51,6 @@ AddFunction BloodDefaultAoEActions
 	if Rune() >= 3 or RunicPower() < 45 Spell(heart_strike)
 	Spell(consumption)
 	Spell(blood_boil)
-	if target.DebuffRefreshable(blood_plague_debuff) Spell(deaths_caress)
 }
 
 AddFunction BloodHealMe
