15097988f043f76f6f34de70ce68822832410048
Xeltor <Xeltor@gmail.com>
2017-09-02 13:39:40 +0200

    Raid interrupts for all current raids.

--- libs/LibInterrupt-1.0/data/legion_raid.lua
+++ libs/LibInterrupt-1.0/data/legion_raid.lua
@@ -37,3 +37,126 @@ InterruptTable["Scorned Sister"] = {
 InterruptTable["Dreadsoul Corruptor"] = {
     ["Shadow Volley"] = true
 }
+
+-- Trial of Valor
+-- Helya
+InterruptTable["Helarjar Mistwatcher"] = {
+    ["Mist Infusion"] = true
+}
+
+-- The Nighthold
+-- Skorpyron
+InterruptTable["Withered Skulker"] = {
+    ["Withering Volley"] = true
+}
+
+-- Chronomatic Anomaly
+InterruptTable["Waning Time Particle"] = {
+    ["Warp Nightwell"] = true
+}
+
+-- Trilliax
+InterruptTable["Putrid Sludge"] = {
+    ["Slop Burst"] = true
+}
+
+-- Spellblade Aluriel
+InterruptTable["Chronowraith"] = {
+    ["Mass Siphon"] = true
+}
+
+InterruptTable["Nighthold Protector"] = {
+    ["Mass Suppress"] = true
+}
+
+InterruptTable["Nobleborn Warpcaster"] = {
+    ["Reverse Wounds"] = true
+}
+
+InterruptTable["Terrace Grove-Tender"] = {
+    ["Shrink"] = true
+}
+
+InterruptTable["Fiery Enchantment"] = {
+    ["Pyroblast"] = true
+}
+
+-- High Botanist Tel'arn
+InterruptTable["Nightborne Sage"] = {
+    ["Time Reversal"] = true
+}
+
+-- Krosus
+InterruptTable["Chaos Mage Beleron"] = {
+    ["Chaotic Energies"] = true
+}
+
+InterruptTable["Burning Ember"] = {
+    ["Felblast"] = true
+}
+
+-- Tichondrius
+InterruptTable["Felsworn Chaos-Mage"] = {
+    ["Eradication"] = true
+}
+
+-- Elisande
+InterruptTable["Astral Farseer"] = {
+    ["Faint Hope"] = true
+}
+
+InterruptTable["Recursive Elemental"] = {
+    ["Blast"] = true,
+    ["Recursive"] = true
+}
+
+InterruptTable["Expedient Elemental"] = {
+    ["Expedite"] = true
+}
+
+InterruptTable["Elisande"] = {
+    ["Ablative Pulse"] = true
+}
+
+-- Gul'dan
+InterruptTable["Azagrim"] = {
+    ["Carrion Wave"] = true
+}
+
+InterruptTable["Beltheris"] = {
+    ["Carrion Wave"] = true
+}
+
+InterruptTable["Dalvengyr"] = {
+    ["Carrion Wave"] = true
+}
+
+-- Tomb of Sargeras
+-- Demonic Inquisition
+InterruptTable["Belac"] = {
+    ["Pangs of Guilt"] = true
+}
+
+-- Harjatan
+InterruptTable["Razorjaw Wavemender"] = {
+    ["Watery Splash"] = true
+}
+
+-- Mistress Sassz'ine
+InterruptTable["Razorjaw Waverunner"] = {
+    ["Water Blast"] = true
+}
+
+InterruptTable["Abyss Stalker"] = {
+    ["Dark Depths"] = true
+}
+
+-- The Desolate Host
+InterruptTable["Fallen Priestess"] = {
+    ["Shattering Scream"] = true
+}
+
+-- Maiden of Vigilance
+InterruptTable["Maiden of Vigilance"] = {
+    ["Wrath of the Creators"] = true
+}
