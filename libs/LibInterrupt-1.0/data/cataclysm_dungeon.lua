755158d083a188684b911eaddda0596e1cd7088d
Xeltor <Xeltor@gmail.com>
2017-09-04 22:06:48 +0200

    Interrupt update for Cataclysm Timewalking.

---
+++ libs/LibInterrupt-1.0/data/cataclysm_dungeon.lua
@@ -0,0 +1,30 @@
+local InterruptTable = _G.InterruptTable
+
+-- End Times
+InterruptTable["Echo of Tyrande"] = {
+    ["Stardust"] = true
+}
+
+-- Grim Batol
+InterruptTable["Faceless Corrupter"] = {
+    ["Umbral Mending"] = true
+}
+
+-- Throne of the Tides
+InterruptTable["Lady Naz'jar"] = {
+    ["Shock Blast"] = true
+}
+
+InterruptTable["Mindbender Ghur'sha"] = {
+    ["Lava Bolt"] = true
+}
+
+InterruptTable["Vicious Mindlasher"] = {
+    ["Shadow Bolt"] = true,
+    ["Brain Spike"] = true
+}
+
+-- The Stone Core
+InterruptTable["High Priestess Azil"] = {
+    ["Force Grip"] = true
+}
