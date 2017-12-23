07a4850df1047f7462cae05360f8105e176a5af9
Kevin De Smet <kevin.de.smet@outlook.be>
2017-12-23 14:06:24 +0100

    dump some more energy when BoB is off cd

--- scripts/ovale_monk.lua
+++ scripts/ovale_monk.lua
@@ -101,7 +101,7 @@ AddFunction BrewmasterDefaultMainActions
 		Spell(blackout_strike)
 		if (target.DebuffPresent(keg_smash_debuff)) Spell(breath_of_fire)
 		if (BuffRefreshable(rushing_jade_wind_buff)) Spell(rushing_jade_wind)
-		if (EnergyDeficit() <= 35) Spell(tiger_palm)
+		if (EnergyDeficit() <= 35 or (Talent(black_ox_talent) and SpellCooldown(black_ox_brew) <= 0)) Spell(tiger_palm)
 		Spell(chi_burst)
 		Spell(chi_wave)
 	}
@@ -136,7 +136,7 @@ AddFunction BrewmasterDefaultAoEActions
 	Spell(chi_wave)
 	if (target.DebuffPresent(keg_smash_debuff) and (not HasEquippedItem(salsalabims_lost_tunic) or not BuffPresent(blackout_combo_buff))) Spell(breath_of_fire)
 	if (BuffRefreshable(rushing_jade_wind_buff)) Spell(rushing_jade_wind)
-	if (EnergyDeficit() <= 35) Spell(tiger_palm)
+	if (EnergyDeficit() <= 35 or (Talent(black_ox_talent) and SpellCooldown(black_ox_brew) <= 0)) Spell(tiger_palm)
 	if (not BuffPresent(blackout_combo_buff)) Spell(blackout_strike)	
 }
 
