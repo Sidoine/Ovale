b93f23f133d67bf149dcd0f6caf6f213dc29198e
Xeltor <Xeltor@gmail.com>
2017-12-31 02:13:54 +0100

    Added Zeks Exterminatus for Priests.

--- dist/scripts/ovale_priest_spells.lua
+++ dist/scripts/ovale_priest_spells.lua
@@ -177,6 +177,8 @@ AddFunction CurrentInsanityDrain {
 #Legendary
 Define(mangazas_madness 132864)
 Define(buff_sephuzs_secret 208052)
+Define(zeks_exterminatus 144438)
+Define(zeks_exterminatus_buff 236545)
 
 #Artifact traits
 Define(to_the_pain 193644)
