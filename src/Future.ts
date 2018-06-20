e9934fe17ca566c4e659f6b390de1314cb01c8b3
Michał Wojtkowski <michal.wojtkowski@gmail.com>
2018-06-20 21:44:17 +0200

    Patch 8.0.1/API changes UNIT_SPELLCAST_START - no longer provide spell name and rank.

--- src/Future.ts
+++ src/Future.ts
@@ -450,10 +450,11 @@ export class OvaleFutureClass extends OvaleFutureBase {
             this.StopProfiling("OvaleFuture_UNIT_SPELLCAST_SENT");
         }
     }
-    UNIT_SPELLCAST_START(event: string, unitId: string, spell: string, rank: number, lineId: number, spellId: number) {
+    UNIT_SPELLCAST_START(event: string, unitId: string, lineId: number, spellId: number) {
         if ((unitId == "player" || unitId == "pet") && !WHITE_ATTACK[spellId]) {
+            let [spell] = GetSpellInfo(spellId);
             this.StartProfiling("OvaleFuture_UNIT_SPELLCAST_START");
-            this.DebugTimestamp(event, unitId, spell, rank, lineId, spellId);
+            this.DebugTimestamp(event, unitId, spell, lineId, spellId);
             let now = GetTime();
             let [spellcast] = this.GetSpellcast(spell, spellId, lineId, now);
             if (spellcast) {
