0d6b19d722ff6102ac19415367ba27a4eac12416
Michał Wojtkowski <michal.wojtkowski@gmail.com>
2018-07-25 10:44:22 +0200

    Import simc Brewmaster profile

--- dist/scripts/ovale_monk.lua
+++ dist/scripts/ovale_monk.lua
@@ -1038,3 +1038,236 @@ AddIcon checkbox=opt_monk_windwalker_aoe help=cd specialization=windwalker
 ]]
     OvaleScripts:RegisterScript("MONK", "windwalker", name, desc, code, "script")
 end
+do
+    local name = "sc_monk_brewmaster_t21"
+    local desc = "[8.0] Simulationcraft: Monk_Brewmaster_T21"
+    local code = [[
+# Based on SimulationCraft profile "T21_Monk_Brewmaster".
+#    class=monk
+#    spec=brewmaster
+#    talents=2020033
+
+Include(ovale_common)
+Include(ovale_trinkets_mop)
+Include(ovale_trinkets_wod)
+Include(ovale_monk_spells)
+
+AddCheckBox(opt_melee_range L(not_in_melee_range) specialization=brewmaster)
+AddCheckBox(opt_use_consumables L(opt_use_consumables) default specialization=brewmaster)
+AddCheckBox(opt_chi_burst SpellName(chi_burst) default specialization=brewmaster)
+
+AddFunction BrewmasterUseItemActions
+{
+ Item(Trinket0Slot text=13 usable=1)
+ Item(Trinket1Slot text=14 usable=1)
+}
+
+AddFunction BrewmasterGetInMeleeRange
+{
+ if CheckBoxOn(opt_melee_range) and not target.InRange(tiger_palm) Texture(misc_arrowlup help=L(not_in_melee_range))
+}
+
+### actions.default
+
+AddFunction BrewmasterDefaultMainActions
+{
+ #keg_smash,if=spell_targets>=3
+ if Enemies() >= 3 Spell(keg_smash)
+ #tiger_palm,if=buff.blackout_combo.up
+ if BuffPresent(blackout_combo_buff) Spell(tiger_palm)
+ #keg_smash
+ Spell(keg_smash)
+ #blackout_strike
+ Spell(blackout_strike)
+ #breath_of_fire,if=buff.blackout_combo.down&(buff.bloodlust.down|(buff.bloodlust.up&&dot.breath_of_fire_dot.refreshable))
+ if BuffExpires(blackout_combo_buff) and { BuffExpires(burst_haste_buff any=1) or BuffPresent(burst_haste_buff any=1) and target.DebuffRefreshable(breath_of_fire_debuff) } Spell(breath_of_fire)
+ #rushing_jade_wind,if=buff.rushing_jade_wind.down
+ if BuffExpires(rushing_jade_wind_buff) Spell(rushing_jade_wind)
+ #chi_burst
+ if CheckBoxOn(opt_chi_burst) Spell(chi_burst)
+ #chi_wave
+ Spell(chi_wave)
+ #tiger_palm,if=!talent.blackout_combo.enabled&cooldown.keg_smash.remains>gcd&(energy+(energy.regen*(cooldown.keg_smash.remains+gcd)))>=55
+ if not Talent(blackout_combo_talent) and SpellCooldown(keg_smash) > GCD() and Energy() + EnergyRegenRate() * { SpellCooldown(keg_smash) + GCD() } >= 55 Spell(tiger_palm)
+}
+
+AddFunction BrewmasterDefaultMainPostConditions
+{
+}
+
+AddFunction BrewmasterDefaultShortCdActions
+{
+ #auto_attack
+ BrewmasterGetInMeleeRange()
+ #purifying_brew,if=stagger.heavy|(stagger.moderate&cooldown.brews.charges_fractional>=cooldown.brews.max_charges-0.5&buff.ironskin_brew.remains>=buff.ironskin_brew.duration*2.5)
+ if DebuffPresent(heavy_stagger_debuff) or DebuffPresent(moderate_stagger_debuff) and SpellCharges(ironskin_brew count=0) >= SpellMaxCharges(ironskin_brew) - 0.5 and BuffRemaining(ironskin_brew_buff) >= BaseDuration(ironskin_brew_buff) * 2.5 Spell(purifying_brew)
+ #ironskin_brew,if=buff.blackout_combo.down&cooldown.brews.charges_fractional>=cooldown.brews.max_charges-0.1-(1+buff.ironskin_brew.remains<=buff.ironskin_brew.duration*0.5)&buff.ironskin_brew.remains<=buff.ironskin_brew.duration*2
+ if BuffExpires(blackout_combo_buff) and SpellCharges(ironskin_brew count=0) >= SpellMaxCharges(ironskin_brew) - 0.1 - { 1 + BuffRemaining(ironskin_brew_buff) <= BaseDuration(ironskin_brew_buff) * 0.5 } and BuffRemaining(ironskin_brew_buff) <= BaseDuration(ironskin_brew_buff) * 2 Spell(ironskin_brew)
+}
+
+AddFunction BrewmasterDefaultShortCdPostConditions
+{
+ Enemies() >= 3 and Spell(keg_smash) or BuffPresent(blackout_combo_buff) and Spell(tiger_palm) or Spell(keg_smash) or Spell(blackout_strike) or BuffExpires(blackout_combo_buff) and { BuffExpires(burst_haste_buff any=1) or BuffPresent(burst_haste_buff any=1) and target.DebuffRefreshable(breath_of_fire_debuff) } and Spell(breath_of_fire) or BuffExpires(rushing_jade_wind_buff) and Spell(rushing_jade_wind) or CheckBoxOn(opt_chi_burst) and Spell(chi_burst) or Spell(chi_wave) or not Talent(blackout_combo_talent) and SpellCooldown(keg_smash) > GCD() and Energy() + EnergyRegenRate() * { SpellCooldown(keg_smash) + GCD() } >= 55 and Spell(tiger_palm)
+}
+
+AddFunction BrewmasterDefaultCdActions
+{
+ #gift_of_the_ox
+ #dampen_harm,if=incoming_damage_1500ms&buff.fortifying_brew.down
+ if IncomingDamage(1.5) > 0 and BuffExpires(fortifying_brew_buff) Spell(dampen_harm)
+ #fortifying_brew,if=incoming_damage_1500ms&(buff.dampen_harm.down|buff.diffuse_magic.down)
+ if IncomingDamage(1.5) > 0 and { BuffExpires(dampen_harm_buff) or BuffExpires(diffuse_magic_buff) } Spell(fortifying_brew)
+ #use_item,name=archimondes_hatred_reborn
+ BrewmasterUseItemActions()
+ #potion
+ if CheckBoxOn(opt_use_consumables) and target.Classification(worldboss) Item(old_war_potion usable=1)
+ #blood_fury
+ Spell(blood_fury_apsp)
+ #berserking
+ Spell(berserking)
+ #lights_judgment
+ Spell(lights_judgment)
+ #invoke_niuzao_the_black_ox,if=target.time_to_die>45
+ if target.TimeToDie() > 45 Spell(invoke_niuzao_the_black_ox)
+ #black_ox_brew,if=incoming_damage_1500ms&stagger.heavy&cooldown.brews.charges_fractional<=0.75
+ if IncomingDamage(1.5) > 0 and DebuffPresent(heavy_stagger_debuff) and SpellCharges(ironskin_brew count=0) <= 0.75 Spell(black_ox_brew)
+ #black_ox_brew,if=(energy+(energy.regen*cooldown.keg_smash.remains))<40&buff.blackout_combo.down&cooldown.keg_smash.up
+ if Energy() + EnergyRegenRate() * SpellCooldown(keg_smash) < 40 and BuffExpires(blackout_combo_buff) and not SpellCooldown(keg_smash) > 0 Spell(black_ox_brew)
+ #arcane_torrent,if=energy<31
+ if Energy() < 31 Spell(arcane_torrent_chi)
+}
+
+AddFunction BrewmasterDefaultCdPostConditions
+{
+ Enemies() >= 3 and Spell(keg_smash) or BuffPresent(blackout_combo_buff) and Spell(tiger_palm) or Spell(keg_smash) or Spell(blackout_strike) or BuffExpires(blackout_combo_buff) and { BuffExpires(burst_haste_buff any=1) or BuffPresent(burst_haste_buff any=1) and target.DebuffRefreshable(breath_of_fire_debuff) } and Spell(breath_of_fire) or BuffExpires(rushing_jade_wind_buff) and Spell(rushing_jade_wind) or CheckBoxOn(opt_chi_burst) and Spell(chi_burst) or Spell(chi_wave) or not Talent(blackout_combo_talent) and SpellCooldown(keg_smash) > GCD() and Energy() + EnergyRegenRate() * { SpellCooldown(keg_smash) + GCD() } >= 55 and Spell(tiger_palm)
+}
+
+### actions.precombat
+
+AddFunction BrewmasterPrecombatMainActions
+{
+ #chi_burst
+ if CheckBoxOn(opt_chi_burst) Spell(chi_burst)
+ #chi_wave
+ Spell(chi_wave)
+}
+
+AddFunction BrewmasterPrecombatMainPostConditions
+{
+}
+
+AddFunction BrewmasterPrecombatShortCdActions
+{
+}
+
+AddFunction BrewmasterPrecombatShortCdPostConditions
+{
+ CheckBoxOn(opt_chi_burst) and Spell(chi_burst) or Spell(chi_wave)
+}
+
+AddFunction BrewmasterPrecombatCdActions
+{
+ #flask
+ #food
+ #augmentation
+ #snapshot_stats
+ #potion
+ if CheckBoxOn(opt_use_consumables) and target.Classification(worldboss) Item(old_war_potion usable=1)
+}
+
+AddFunction BrewmasterPrecombatCdPostConditions
+{
+ CheckBoxOn(opt_chi_burst) and Spell(chi_burst) or Spell(chi_wave)
+}
+
+### Brewmaster icons.
+
+AddCheckBox(opt_monk_brewmaster_aoe L(AOE) default specialization=brewmaster)
+
+AddIcon checkbox=!opt_monk_brewmaster_aoe enemies=1 help=shortcd specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatShortCdActions()
+ unless not InCombat() and BrewmasterPrecombatShortCdPostConditions()
+ {
+  BrewmasterDefaultShortCdActions()
+ }
+}
+
+AddIcon checkbox=opt_monk_brewmaster_aoe help=shortcd specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatShortCdActions()
+ unless not InCombat() and BrewmasterPrecombatShortCdPostConditions()
+ {
+  BrewmasterDefaultShortCdActions()
+ }
+}
+
+AddIcon enemies=1 help=main specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatMainActions()
+ unless not InCombat() and BrewmasterPrecombatMainPostConditions()
+ {
+  BrewmasterDefaultMainActions()
+ }
+}
+
+AddIcon checkbox=opt_monk_brewmaster_aoe help=aoe specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatMainActions()
+ unless not InCombat() and BrewmasterPrecombatMainPostConditions()
+ {
+  BrewmasterDefaultMainActions()
+ }
+}
+
+AddIcon checkbox=!opt_monk_brewmaster_aoe enemies=1 help=cd specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatCdActions()
+ unless not InCombat() and BrewmasterPrecombatCdPostConditions()
+ {
+  BrewmasterDefaultCdActions()
+ }
+}
+
+AddIcon checkbox=opt_monk_brewmaster_aoe help=cd specialization=brewmaster
+{
+ if not InCombat() BrewmasterPrecombatCdActions()
+ unless not InCombat() and BrewmasterPrecombatCdPostConditions()
+ {
+  BrewmasterDefaultCdActions()
+ }
+}
+
+### Required symbols
+# dampen_harm
+# fortifying_brew_buff
+# fortifying_brew
+# dampen_harm_buff
+# diffuse_magic_buff
+# old_war_potion
+# blood_fury_apsp
+# berserking
+# lights_judgment
+# invoke_niuzao_the_black_ox
+# purifying_brew
+# heavy_stagger_debuff
+# moderate_stagger_debuff
+# ironskin_brew
+# ironskin_brew_buff
+# blackout_combo_buff
+# black_ox_brew
+# keg_smash
+# arcane_torrent_chi
+# tiger_palm
+# blackout_strike
+# breath_of_fire
+# breath_of_fire_debuff
+# rushing_jade_wind
+# rushing_jade_wind_buff
+# chi_burst
+# chi_wave
+# blackout_combo_talent
+
+]]
+    OvaleScripts:RegisterScript("MONK", "brewmaster", name, desc, code, "script")
+end
