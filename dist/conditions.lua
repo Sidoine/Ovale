71a8829080851b2d68fa024b6a5bc68610adf35c
Michał Wojtkowski <michal.wojtkowski@gmail.com>
2018-08-11 20:28:23 +0200

    Add MaxArcaneCharges function

--- dist/conditions.lua
+++ dist/conditions.lua
@@ -1545,7 +1545,11 @@ local function MaxShadowOrbs(positionalParams, namedParams, state, atTime)
 local function MaxSoulShards(positionalParams, namedParams, state, atTime)
         return MaxPower("soulshards", positionalParams, namedParams, state, atTime)
     end
+local function MaxArcaneCharges(positionalParams, namedParams, state, atTime)
+        return MaxPower("arcanecharges", positionalParams, namedParams, state, atTime)
+    end
     OvaleCondition:RegisterCondition("maxalternatepower", false, MaxAlternatePower)
+    OvaleCondition:RegisterCondition("maxarcanecharges", false, MaxArcaneCharges)
     OvaleCondition:RegisterCondition("maxchi", false, MaxChi)
     OvaleCondition:RegisterCondition("maxcombopoints", false, MaxComboPoints)
     OvaleCondition:RegisterCondition("maxenergy", false, MaxEnergy)
