ef992e81e0547894ee1b4586c96e50f02cfcd9d5
Kevin De Smet <kevin.de.smet@outlook.be>
2018-08-16 15:11:06 +0200

    some more tweaks to ISB min

--- dist/scripts/ovale_monk.lua
+++ dist/scripts/ovale_monk.lua
@@ -49,7 +49,7 @@ AddFunction BrewmasterRangeCheck
 AddFunction BrewmasterDefaultShortCDActions
 {
 	# keep ISB up always when taking dmg
-    if (BaseDuration(light_stagger_debuff)-DebuffRemaining(any_stagger_debuff)<3 and BuffExpires(ironskin_brew_buff 3) and BuffExpires(blackout_combo_buff)) Spell(ironskin_brew text=min)
+    if ((BaseDuration(light_stagger_debuff)-DebuffRemaining(any_stagger_debuff)<5 or target.TargetIsPlayer()) and BuffExpires(ironskin_brew_buff 3) and BuffExpires(blackout_combo_buff)) Spell(ironskin_brew text=min)
 	
 	# keep stagger below 100% (or 30% when BOB is up)
 	if (StaggerPercentage() >= 100 or (StaggerPercentage() >= 30 and Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 0)) Spell(purifying_brew)
@@ -74,7 +74,7 @@ AddFunction BrewmasterDefaultShortCDActions
 		if (StaggerPercentage() >= 60 and (BuffRemaining(ironskin_brew_buff) >= 2*BaseDuration(ironskin_brew_buff))) Spell(purifying_brew)
 
 		# always bank 1 charge (or bank 2 with light_brewing)
-		if (SpellCharges(ironskin_brew) > SpellMaxCharges(ironskin_brew)-2)
+		unless (SpellCharges(ironskin_brew) <= SpellMaxCharges(ironskin_brew)-2)
 		{
             # keep ISB rolling
             if BuffRemaining(ironskin_brew_buff) < DebuffRemaining(any_stagger_debuff) and BuffExpires(blackout_combo_buff) Spell(ironskin_brew)
