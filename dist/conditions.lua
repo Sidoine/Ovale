21214756aa7a6805b17d2c49d4021672d322d006
Hemario <kevin.de.smet@outlook.be>
2018-12-18 21:48:51 +0100

    update toc version

--- dist/conditions.lua
+++ dist/conditions.lua
@@ -1320,7 +1320,7 @@ do
 local function PTR(positionalParams, namedParams, atTime)
         local comparator, limit = positionalParams[1], positionalParams[2]
         local _, _, _, uiVersion = GetBuildInfo()
-        local value = (uiVersion > 80000) and 1 or 0
+        local value = (uiVersion > 80100) and 1 or 0
         return Compare(value, comparator, limit)
     end
     OvaleCondition:RegisterCondition("ptr", false, PTR)
