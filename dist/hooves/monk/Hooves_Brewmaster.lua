bf7d3277b86fa28557c097bee400836437d75414
LookAtMyHooves <30540002+LookAtMyHooves@users.noreply.github.com>
2019-09-25 16:13:56 +0100

    Update Hooves_Brewmaster.lua

--- dist/hooves/monk/Hooves_Brewmaster.lua
+++ dist/hooves/monk/Hooves_Brewmaster.lua
@@ -97,7 +97,6 @@ AddFunction BrewmasterDefaultMainActions
  Spell(blackout_strike)
  #keg_smash
  Spell(keg_smash)
- #
  #concentrated_flame,if=dot.concentrated_flame.remains=0
  if not target.DebuffPresent(concentrated_flame_burn_debuff) Spell(concentrated_flame_essence)
  #expel_harm,if=buff.gift_of_the_ox.stack>=3
