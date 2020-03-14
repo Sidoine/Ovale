ca4e859354ff81632af9c3237650aeeffe0511da
LookAtMyHooves <30540002+LookAtMyHooves@users.noreply.github.com>
2020-03-14 20:36:33 +0000

    mistweaver

--- dist/hooves/index.lua
+++ dist/hooves/index.lua
@@ -29,6 +29,7 @@ local registerPriestDisciplineToast = __ovale_priest.registerPriestDisciplineToa
 local registerDruidBalanceHooves = __ovale_druid.registerDruidBalanceHooves
 local registerMonkWindwalkerHooves = __ovale_monk.registerMonkWindwalkerHooves
 local registerDruidFeralToast83 = __ovale_druid.registerDruidFeralToast83
+local registerMonkMistweaverHooves = __ovale_monk.registerMonkMistweaverHooves
 __exports.registerScripts = function(ovaleScripts)
 	registerMonkBrewmasterHooves(ovaleScripts)
 	registerDruidFeralToast(ovaleScripts)
@@ -43,6 +44,7 @@ __exports.registerScripts = function(ovaleScripts)
 --	registerDruidRestorationHooves(ovaleScripts)
 		registerMonkWindwalkerHooves(ovaleScripts)
 		registerDruidFeralToast83(ovaleScripts)
+		registerMonkMistweaverHooves(ovaleScripts)
 	-- For each created register link it here to ovaleScripts.
 	-- Example:
 	-- registerDruidFeralHooves(ovaleScripts)
