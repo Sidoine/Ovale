6f58fb3ab1ba9caee81b45393a122d786be00fdc
Johnny C. Lam <jlam88@gmail.com>
2020-12-30 18:43:07 -0500

    fix(deathknight): mark totem spells and fix sacrificial_pact
    Raise Dead and Summon Gargoyle are totem spells, and Sacrificial
    Pact can only be cast if a ghoul is active.

--- src/scripts/ovale_deathknight_spells.ts
+++ src/scripts/ovale_deathknight_spells.ts
@@ -437,6 +437,15 @@ Define(lead_by_example_soulbind 342156)
     `;
     // END
 
+    code += `
+# raise_dead
+  SpellInfo(raise_dead totem=1)
+# sacrificial_pact
+  SpellRequire(sacrificial_pact unusable set=1 enabled=((specialization(unholy) and not pet.present()) or (not specialization(unholy) and totemexpires(raise_dead))))
+# summon_gargoyle
+  SpellInfo(summon_gargoyle totem=1)
+`;
+
     OvaleScripts.RegisterScript(
         "DEATHKNIGHT",
         undefined,
