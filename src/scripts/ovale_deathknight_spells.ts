9310b3fd20f7a2c7614e5bc684475f99c1781c1b
Johnny C. Lam <jlam88@gmail.com>
2021-01-02 03:41:06 -0500

    fix(deathknight): raise_dead_unholy can't be used with a pet active

--- src/scripts/ovale_deathknight_spells.ts
+++ src/scripts/ovale_deathknight_spells.ts
@@ -440,6 +440,8 @@ Define(lead_by_example_soulbind 342156)
     code += `
 # raise_dead
   SpellInfo(raise_dead totem=1)
+# raise_dead_unholy
+  SpellRequire(raise_dead_unholy unusable set=1 enabled=(pet.present()))
 # sacrificial_pact
   SpellRequire(sacrificial_pact unusable set=1 enabled=((specialization(unholy) and not pet.present()) or (not specialization(unholy) and totemexpires(raise_dead))))
 # summon_gargoyle
