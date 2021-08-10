693eb93861935627e0da551ab6df0b9e5b3829fa
Johnny C. Lam <jlam88@gmail.com>
2021-08-10 15:36:11 -0400

    refactor: deque, queue, and stack classes
    Add implmementations of deque, queue, and stack ADTs using a ring
    buffer that can grow its maximum capacity. The API of the base
    Deque class is based on the JavaScript Array API.
    
    Also add a testsuite for the new classes.

--- src/tools/Queue.ts
+++ src/tools/Queue.ts
@@ -1,4 +1,306 @@
+import { LuaArray, ipairs, lualength } from "@wowts/lua";
 import { format } from "@wowts/string";
+
+interface Iterator<T> {
+    value: T;
+    next(): boolean;
+}
+
+class DequeBackToFrontIterator<T> implements Iterator<T> {
+    public value!: T;
+    private index = 0;
+    private remaining = 0;
+
+    constructor(private deque: Deque<T>) {
+        this.index = deque.indexAfter(deque.last);
+        this.remaining = deque.length;
+    }
+
+    next() {
+        if (this.deque.length == 0) {
+            return false;
+        }
+        this.index = this.deque.indexBefore(this.index);
+        this.value = this.deque.buffer[this.index];
+        this.remaining -= 1;
+        return this.remaining >= 0;
+    }
+}
+
+class DequeFrontToBackIterator<T> implements Iterator<T> {
+    public value!: T;
+    private index = 0;
+    private remaining = 0;
+
+    constructor(private deque: Deque<T>) {
+        this.index = deque.indexBefore(deque.first);
+        this.remaining = deque.length;
+    }
+
+    next() {
+        if (this.deque.length == 0) {
+            return false;
+        }
+        this.index = this.deque.indexAfter(this.index);
+        this.value = this.deque.buffer[this.index];
+        this.remaining -= 1;
+        return this.remaining >= 0;
+    }
+}
+
+// Double-ended queue
+export class Deque<T> {
+    capacity = 0;
+    private canGrow = true;
+
+    buffer: LuaArray<T> = {};
+    first = 0;
+    last = 0;
+    length = 0;
+
+    constructor(capacity?: number, fixed?: boolean) {
+        this.capacity = (capacity && capacity > 0 && capacity) || 1;
+        this.canGrow = fixed == undefined || !fixed;
+    }
+
+    isEmpty() {
+        return this.length == 0;
+    }
+
+    isFull() {
+        return !this.canGrow && this.length == this.capacity;
+    }
+
+    front() {
+        return (this.length > 0 && this.buffer[this.first]) || undefined;
+    }
+
+    back() {
+        return (this.length > 0 && this.buffer[this.last]) || undefined;
+    }
+
+    indexAfter = (index: number) => {
+        return (index < this.capacity && index + 1) || 1;
+    };
+
+    indexBefore = (index: number) => {
+        return (index > 1 && index - 1) || this.capacity;
+    };
+
+    backToFrontIterator() {
+        return new DequeBackToFrontIterator<T>(this);
+    }
+
+    frontToBackIterator() {
+        return new DequeFrontToBackIterator<T>(this);
+    }
+
+    fromArray(t: LuaArray<T>) {
+        const length = lualength(t);
+        if (length > 0) {
+            const buffer = this.buffer;
+            for (const [i, value] of ipairs(t)) {
+                buffer[i] = value;
+            }
+            if (this.capacity < length) {
+                this.capacity = length;
+            }
+            this.first = 1;
+            this.last = length;
+            this.length = length;
+        } else {
+            this.first = 0;
+            this.last = 0;
+            this.length = 0;
+        }
+    }
+
+    asArray(reverse?: boolean) {
+        const t: LuaArray<T> = {};
+        let index = 1;
+        const iterator =
+            (reverse == true && this.backToFrontIterator()) ||
+            this.frontToBackIterator();
+        while (iterator.next()) {
+            t[index] = iterator.value;
+            index += 1;
+        }
+        return t;
+    }
+
+    at(index: number) {
+        if (1 <= index && index <= this.length) {
+            index = this.first + index - 1;
+            if (index > this.capacity) {
+                index -= this.capacity;
+            }
+            return this.buffer[index];
+        }
+        return undefined;
+    }
+
+    removeAt(index: number) {
+        if (index == 1) {
+            this.shift();
+        } else if (index == this.length) {
+            this.pop();
+        } else if (1 < index && index < this.length) {
+            const buffer = this.buffer;
+            const first = this.first;
+            const last = this.last;
+            const length = this.length;
+            const mappedIndex = first + index - 1;
+            if (last > first) {
+                // 1 [2 3 4] 5
+                if (index - 1 > length - index) {
+                    // [1 .. (index - 1)] is longer than [(index + 1) .. length]
+                    let i = mappedIndex;
+                    while (i < last) {
+                        buffer[i] = buffer[i + 1];
+                        i += 1;
+                    }
+                    delete buffer[last];
+                    this.last -= 1;
+                } else {
+                    let i = mappedIndex;
+                    while (i > first) {
+                        buffer[i] = buffer[i - 1];
+                        i -= 1;
+                    }
+                    delete buffer[first];
+                    this.first += 1;
+                }
+            } else {
+                // 1 2] 3 [4 5
+                if (mappedIndex > this.capacity) {
+                    let i = mappedIndex - this.capacity;
+                    while (i < last) {
+                        buffer[i] = buffer[i + 1];
+                        i += 1;
+                    }
+                    delete buffer[last];
+                    this.last -= 1;
+                } else {
+                    let i = mappedIndex;
+                    while (i > first) {
+                        buffer[i] = buffer[i - 1];
+                        i -= 1;
+                    }
+                    delete buffer[first];
+                    this.first += 1;
+                }
+            }
+            this.length -= 1;
+        }
+    }
+
+    private grow(capacity?: number) {
+        capacity = capacity || 2 * this.capacity;
+        if (capacity > this.capacity && this.last < this.first) {
+            // shift [first, first + 1, ... , capacity] elements to the right
+            const shift = capacity - this.capacity;
+            const buffer = this.buffer;
+            const first = this.first;
+            let i = this.capacity;
+            while (i >= first) {
+                buffer[i + shift] = buffer[i];
+                delete buffer[i];
+                i -= 1;
+            }
+            this.first = this.first + shift;
+        }
+        this.capacity = capacity;
+    }
+
+    push(value: T) {
+        if (this.length == 0) {
+            this.length = 1;
+            this.first = 1;
+            this.last = 1;
+            this.buffer[1] = value;
+        } else {
+            if (this.length < this.capacity) {
+                this.length += 1;
+            } else if (this.canGrow) {
+                this.grow();
+                this.length += 1;
+            } else {
+                this.first = this.indexAfter(this.first);
+            }
+            this.last = this.indexAfter(this.last);
+            this.buffer[this.last] = value;
+        }
+    }
+
+    pop() {
+        if (this.length > 0) {
+            const value = this.buffer[this.last];
+            if (this.length == 1) {
+                this.length = 0;
+                this.first = 0;
+                this.last = 0;
+            } else {
+                this.length -= 1;
+                this.last = this.indexBefore(this.last);
+            }
+            return value;
+        }
+        return undefined;
+    }
+
+    unshift(value: T) {
+        if (this.length == 0) {
+            this.length = 1;
+            this.first = 1;
+            this.last = 1;
+            this.buffer[1] = value;
+        } else {
+            if (this.length < this.capacity) {
+                this.length += 1;
+            } else if (this.canGrow) {
+                this.grow();
+                this.length += 1;
+            } else {
+                this.last = this.indexBefore(this.last);
+            }
+            this.first = this.indexBefore(this.first);
+            this.buffer[this.first] = value;
+        }
+    }
+
+    shift() {
+        if (this.length > 0) {
+            const value = this.buffer[this.first];
+            if (this.length == 1) {
+                this.length = 0;
+                this.first = 0;
+                this.last = 0;
+            } else {
+                this.first = this.indexAfter(this.first);
+                this.length -= 1;
+            }
+            return value;
+        }
+        return undefined;
+    }
+}
+
+export class Queue<T> extends Deque<T> {
+    iterator() {
+        return this.frontToBackIterator();
+    }
+}
+
+// Stack (FIFO)
+export class Stack<T> extends Deque<T> {
+    top() {
+        return super.back();
+    }
+
+    iterator() {
+        return this.backToFrontIterator();
+    }
+}
 
 class BackToFrontIterator<T> {
     public value!: T;
---
+++ src/tools/queue.spec.ts
@@ -0,0 +1,435 @@
+import { expect, test } from "@jest/globals";
+import { Deque } from "./Queue";
+
+test("new queue", () => {
+    const q = new Deque<number>();
+    expect(q.capacity).toBe(1);
+});
+
+test("new queue sets size", () => {
+    const q = new Deque<number>(10);
+    expect(q.capacity).toBe(10);
+});
+
+test("new queue is empty", () => {
+    const q = new Deque<number>();
+    expect(q.isEmpty()).toBe(true);
+    expect(q.length).toBe(0);
+    expect(q.front()).toBe(undefined);
+    expect(q.back()).toBe(undefined);
+});
+
+test("new queue is not full", () => {
+    const q = new Deque<number>();
+    expect(q.isFull()).toBe(false);
+});
+
+test("push onto empty queue", () => {
+    const q = new Deque<number>();
+    q.push(10);
+    expect(q.length).toBe(1);
+    expect(q.back()).toBe(10);
+});
+
+test("from empty array", () => {
+    const q = new Deque<number>();
+    q.fromArray({});
+    expect(q.length).toBe(0);
+    expect(q.isEmpty()).toBe(true);
+    expect(q.asArray()).toEqual({});
+});
+
+test("from one-element array", () => {
+    const q = new Deque<number>();
+    const expected = { 1: 10 };
+    q.fromArray(expected);
+    expect(q.length).toBe(1);
+    expect(q.back()).toBe(10);
+    expect(q.asArray()).toEqual(expected);
+});
+
+test("from two-element array", () => {
+    const q = new Deque<number>();
+    const expected = { 1: 10, 2: 20 };
+    q.fromArray(expected);
+    expect(q.capacity).toBe(2);
+    expect(q.length).toBe(2);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(20);
+    expect(q.asArray()).toEqual(expected);
+});
+
+test("as array", () => {
+    const q = new Deque<number>();
+    const t = { 1: 10, 2: 20, 3: 30 };
+    q.fromArray(t);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+    expect(q.asArray(true)).toEqual({ 1: 30, 2: 20, 3: 10 });
+});
+
+test("unshift empty queue", () => {
+    const q = new Deque<number>();
+    q.unshift(10);
+    expect(q.length).toBe(1);
+    expect(q.front()).toBe(10);
+    expect(q.asArray()).toEqual({ 1: 10 });
+});
+
+test("one-element queue has same front and back", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10 });
+    expect(q.length).toBe(1);
+    expect(q.front()).toBe(q.back());
+});
+
+test("pop from one-element queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10 });
+    expect(q.pop()).toBe(10);
+    expect(q.length).toBe(0);
+    expect(q.isEmpty()).toBe(true);
+});
+
+test("shift one-element queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10 });
+    expect(q.shift()).toBe(10);
+    expect(q.length).toBe(0);
+    expect(q.isEmpty()).toBe(true);
+});
+
+test("remove at 1 of one-element queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10 });
+    q.removeAt(1);
+    expect(q.length).toBe(0);
+    expect(q.isEmpty()).toBe(true);
+});
+
+test("push onto queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20 });
+    q.push(30);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+});
+
+test("unshift queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20 });
+    q.unshift(30);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(30);
+    expect(q.back()).toBe(20);
+    expect(q.asArray()).toEqual({ 1: 30, 2: 10, 3: 20 });
+});
+
+test("pop from queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30 });
+    const value = q.pop();
+    expect(q.length).toBe(2);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(20);
+    expect(value).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20 });
+});
+
+test("shift queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30 });
+    const value = q.shift();
+    expect(q.length).toBe(2);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(30);
+    expect(value).toBe(10);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30 });
+});
+
+test("remove at 1 of queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(1);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40 });
+});
+
+test("remove at end of queue", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(q.length);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+});
+
+test("remove at middle of queue near front", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(2);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 30, 3: 40 });
+});
+
+test("remove at middle of queue near back", () => {
+    const q = new Deque<number>();
+    q.fromArray({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(3);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 40 });
+});
+
+test("grow queue", () => {
+    const q = new Deque<number>();
+    q.push(10);
+    expect(q.capacity).toBe(1);
+    q.push(20); // grow once
+    expect(q.capacity).toBe(2);
+    q.push(30); // grow twice
+    expect(q.capacity).toBe(4);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+});
+
+test("not full fixed queue", () => {
+    const q = new Deque<number>(3, true);
+    q.fromArray({ 1: 10, 2: 20 });
+    expect(q.capacity).toBe(3);
+    expect(q.length).toBe(2);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(20);
+    expect(q.isFull()).toBe(false);
+});
+
+test("full fixed queue", () => {
+    const q = new Deque<number>(3, true);
+    q.fromArray({ 1: 10, 2: 20, 3: 30 });
+    expect(q.capacity).toBe(3);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.isFull()).toBe(true);
+});
+
+test("push onto full queue", () => {
+    const q = new Deque<number>(3, true);
+    q.fromArray({ 1: 10, 2: 20, 3: 30 });
+    q.push(40);
+    expect(q.capacity).toBe(3);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(40);
+    expect(q.isFull()).toBe(true);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40 });
+});
+
+test("unshift full queue", () => {
+    const q = new Deque<number>(3, true);
+    q.fromArray({ 1: 10, 2: 20, 3: 30 });
+    q.unshift(40);
+    expect(q.capacity).toBe(3);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(40);
+    expect(q.back()).toBe(20);
+    expect(q.isFull()).toBe(true);
+    expect(q.asArray()).toEqual({ 1: 40, 2: 10, 3: 20 });
+});
+
+function createWraparoundQueue(fixed?: boolean) {
+    /* Return a 4-element queue with capacity 5 that starts in the
+     * middle of the buffer.
+     */
+    if (fixed == undefined) {
+        fixed = false;
+    }
+    const q = new Deque<number>(5, fixed);
+    q.push(-20);
+    q.push(-10);
+    q.push(0);
+    q.push(10);
+    q.push(20);
+    q.shift(); // remove -20
+    q.shift(); // remove -10
+    q.shift(); // remove 0
+    q.push(30);
+    q.push(40);
+    // queue.buffer: 30 40] nil [10 20
+    //expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    return q;
+}
+
+test("queue with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    expect(q.length).toBe(4);
+    expect(q.capacity).toBe(5);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+});
+
+test("push onto queue with wrapround indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(5);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(50);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40, 5: 50 });
+});
+
+test("unshift queue with wrapround indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.unshift(50);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(5);
+    expect(q.front()).toBe(50);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 50, 2: 10, 3: 20, 4: 30, 5: 40 });
+});
+
+test("pop from queue with wrapround indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    expect(q.pop()).toBe(40);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+});
+
+test("shift queue with wrapround indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    expect(q.shift()).toBe(10);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40 });
+});
+
+test("remove at 1 of queue with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(1);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40 });
+});
+
+test("remove at end of queue with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(q.length);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(30);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30 });
+});
+
+test("remove at middle of queue near front with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(2);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 30, 3: 40 });
+});
+
+test("remove at middle of queue near back with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.removeAt(3);
+    expect(q.length).toBe(3);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 40 });
+});
+
+test("grow queue with wraparound indexing", () => {
+    const q = createWraparoundQueue();
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    q.push(60);
+    expect(q.length).toBe(6);
+    expect(q.capacity).toBe(10);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(60);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60 });
+});
+
+test("full queue with wraparound indexing", () => {
+    const q = createWraparoundQueue(true);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(5);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(50);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40, 5: 50 });
+});
+
+test("push onto full queue with wraparound indexing", () => {
+    const q = createWraparoundQueue(true);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    q.push(60);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(5);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(60);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40, 4: 50, 5: 60 });
+});
+
+test("unshift full queue with wraparound indexing", () => {
+    const q = createWraparoundQueue(true);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    q.unshift(60);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(5);
+    expect(q.front()).toBe(60);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 60, 2: 10, 3: 20, 4: 30, 5: 40 });
+});
+
+test("pop from full queue with wraparound indexing", () => {
+    const q = createWraparoundQueue(true);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    expect(q.pop()).toBe(50);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(4);
+    expect(q.front()).toBe(10);
+    expect(q.back()).toBe(40);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+});
+
+test("shift from full queue with wraparound indexing", () => {
+    const q = createWraparoundQueue(true);
+    expect(q.asArray()).toEqual({ 1: 10, 2: 20, 3: 30, 4: 40 });
+    q.push(50); // buffer at full capacity
+    expect(q.shift()).toBe(10);
+    expect(q.capacity).toBe(5);
+    expect(q.length).toBe(4);
+    expect(q.front()).toBe(20);
+    expect(q.back()).toBe(50);
+    expect(q.asArray()).toEqual({ 1: 20, 2: 30, 3: 40, 4: 50 });
+});
